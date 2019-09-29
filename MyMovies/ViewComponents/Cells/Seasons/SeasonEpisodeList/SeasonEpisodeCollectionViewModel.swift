5f6c063530ab090251cf22608712c65923dbd666
Jeans <jeansruiz.c@gmail.com>
2019-09-29 02:46:20 -0500

    Implementar Scene para mostrar Seasons, ViewModel de CollectionView

---
+++ MyMovies/ViewComponents/Cells/Seasons/SeasonEpisodeList/SeasonEpisodeCollectionViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  SeasonEpisodeCollectionViewModel.swift
+//  MyTvShows
+//
+//  Created by Jeans on 9/24/19.
+//  Copyright © 2019 Jeans. All rights reserved.
+//
+
+import Foundation
+
+final class SeasonEpisodeCollectionViewModel{
+    
+    //var season:Int
+    //var isSelected = false
+    var seasonNumber: String?
+
+    init( seasonNumber: Int){
+        self.seasonNumber = String(seasonNumber)
+    }
+    
+//    init( seasonNumber: Int){
+//        self.season = seasonNumber
+//        self.seasonNumber = String(seasonNumber)
+//    }
+}

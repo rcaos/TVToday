9bb54c5d4db11fb5430cd815a3036d0479de0c23
Jeans Ruiz <jeansruiz.c@gmail.com>
2020-03-26 00:36:37 -0500

    Fix empty View when Search a TVShow

--- MyMovies/Presentation/SearchScene/View/SearchResults/ResultsSearchViewController.swift
+++ MyMovies/Presentation/SearchScene/View/SearchResults/ResultsSearchViewController.swift
@@ -9,139 +9,140 @@
 import UIKit
 
 protocol ResultsSearchViewControllerDelegate: class {
-    func resultsSearchViewController(_ resultsSearchViewController: ResultsSearchViewController, didSelectedMovie movie: Int )
+  func resultsSearchViewController(_ resultsSearchViewController: ResultsSearchViewController, didSelectedMovie movie: Int )
 }
 
 class ResultsSearchViewController: UIViewController {
+  
+  var resultView: ResultListView = ResultListView()
+  
+  var delegate:ResultsSearchViewControllerDelegate?
+  
+  var loadingView: UIView!
+  
+  var viewModel: ResultsSearchViewModel
+  
+  init(viewModel: ResultsSearchViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError()
+  }
+  
+  override func loadView() {
+    super.loadView()
+    view = resultView
+  }
+  
+  //MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    var resultView: ResultListView = ResultListView()
+    setupTable()
+    setupViewModel()
+  }
+  
+  func setupTable() {
+    resultView.tableView.dataSource = self
+    resultView.tableView.delegate = self
     
-    var delegate:ResultsSearchViewControllerDelegate?
+    let nibName = UINib(nibName: "TVShowViewCell", bundle: nil)
+    resultView.tableView.register(nibName, forCellReuseIdentifier: "TVShowViewCell")
     
-    var loadingView: UIView!
+    buildLoadingView()
+  }
+  
+  //MARK: - SetupViewModel
+  
+  func setupViewModel() {
     
-    var viewModel: ResultsSearchViewModel
-    
-    init(viewModel: ResultsSearchViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
+    viewModel.viewState.observe(on: self) {[weak self] state in
+      self?.configView(with: state)
     }
+  }
+  
+  func configView(with state: SimpleViewState<TVShow>) {
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError()
+    let tableView = resultView.tableView
+    
+    switch state {
+    case .populated(_):
+      tableView.tableFooterView = UIView()
+      tableView.separatorStyle = .singleLine
+      tableView.reloadData()
+    case .empty:
+      tableView.tableFooterView = buildEmptyView()
+      tableView.separatorStyle = .none
+      tableView.reloadData()
+    case .paging(_, _):
+      tableView.tableFooterView = loadingView
+      tableView.separatorStyle = .singleLine
+      tableView.reloadData()
+    default:
+      tableView.tableFooterView = loadingView
     }
+  }
+  
+  func buildEmptyView() -> UIView {
+    let frame = CGRect(x: 0, y: 0, width: resultView.tableView.frame.width, height: 100)
+    let nib = UINib(nibName: "CustomFooterView", bundle: nil)
     
-    override func loadView() {
-        super.loadView()
-        view = resultView
-    }
+    let emptyView = nib.instantiate(withOwner: nil, options: nil).first as! CustomFooterView
+    emptyView.frame = frame
     
-    //MARK: - Life Cycle
+    emptyView.messageLabel.text = "No results to show"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupTable()
-        setupViewModel()
-    }
+    return emptyView
+  }
+  
+  func buildLoadingView() {
+    let defaultFrame = CGRect(x: 0, y: 0, width: resultView.tableView.frame.width, height: 100)
     
-    func setupTable() {
-        resultView.tableView.dataSource = self
-        resultView.tableView.delegate = self
-        
-        let nibName = UINib(nibName: "TVShowViewCell", bundle: nil)
-        resultView.tableView.register(nibName, forCellReuseIdentifier: "TVShowViewCell")
-        
-        buildLoadingView()
-    }
+    let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
+    activityIndicator.color = .darkGray
+    activityIndicator.frame = defaultFrame
     
-    //MARK: - SetupViewModel
+    loadingView = UIView(frame: defaultFrame)
+    loadingView.backgroundColor = .white
     
-    func setupViewModel() {
-        
-        viewModel.viewState.observe(on: self) {[weak self] state in
-            self?.configView(with: state)
-        }
-    }
-    
-    func configView(with state: SimpleViewState<TVShow>) {
-        
-        let tableView = resultView.tableView
-        
-        switch state {
-        case .populated(_):
-            tableView.tableFooterView = UIView()
-            tableView.separatorStyle = .singleLine
-            tableView.reloadData()
-        case .empty:
-            tableView.tableFooterView = buildEmptyView()
-            tableView.separatorStyle = .none
-        case .paging(_, _):
-            tableView.tableFooterView = loadingView
-            tableView.separatorStyle = .singleLine
-            tableView.reloadData()
-        default:
-            tableView.tableFooterView = loadingView
-        }
-    }
-    
-    func buildEmptyView() -> UIView {
-        let frame = CGRect(x: 0, y: 0, width: resultView.tableView.frame.width, height: 100)
-        let nib = UINib(nibName: "CustomFooterView", bundle: nil)
-        
-        let emptyView = nib.instantiate(withOwner: nil, options: nil).first as! CustomFooterView
-        emptyView.frame = frame
-        
-        emptyView.messageLabel.text = "No results to show"
-        
-        return emptyView
-    }
-    
-    func buildLoadingView() {
-        let defaultFrame = CGRect(x: 0, y: 0, width: resultView.tableView.frame.width, height: 100)
-        
-        let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
-        activityIndicator.color = .darkGray
-        activityIndicator.frame = defaultFrame
-        
-        loadingView = UIView(frame: defaultFrame)
-        loadingView.backgroundColor = .white
-        
-        activityIndicator.center = loadingView.center
-        loadingView.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
-    }
+    activityIndicator.center = loadingView.center
+    loadingView.addSubview(activityIndicator)
+    activityIndicator.startAnimating()
+  }
 }
 
 //MARK: - UITableViewDataSource
 
 extension ResultsSearchViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.viewState.value.currentEntities.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TVShowViewCell", for: indexPath) as! TVShowViewCell
+    cell.viewModel = viewModel.getModelFor(indexPath.row)
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.viewState.value.currentEntities.count
+    if case .paging(_, let nextPage) = viewModel.viewState.value,
+      indexPath.row == viewModel.viewState.value.currentEntities.count - 1 {
+      viewModel.searchShows(for: nextPage)
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TVShowViewCell", for: indexPath) as! TVShowViewCell
-        cell.viewModel = viewModel.getModelFor(indexPath.row)
-        
-        if case .paging(_, let nextPage) = viewModel.viewState.value,
-            indexPath.row == viewModel.viewState.value.currentEntities.count - 1 {
-            viewModel.searchShows(for: nextPage)
-        }
-        
-        return cell
-    }
+    return cell
+  }
 }
 
 //MARK: - UITableViewDelegate
 
 extension ResultsSearchViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let selectedShow = viewModel.shows[indexPath.row]
+    delegate?.resultsSearchViewController(self, didSelectedMovie: selectedShow.id)
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedShow = viewModel.shows[indexPath.row]
-        delegate?.resultsSearchViewController(self, didSelectedMovie: selectedShow.id)
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
 }
--- MyMovies/Presentation/ShowsViewModel.swift
+++ MyMovies/Presentation/ShowsViewModel.swift
@@ -9,61 +9,62 @@
 import Foundation
 
 protocol ShowsViewModel: class {
-    
-    associatedtype TVShowCellViewModel
-    
-    var fetchTVShowsUseCase: FetchTVShowsUseCase { get set }
-    
-    var filter: TVShowsListFilter { get set }
-    
-    var viewState: Observable<SimpleViewState<TVShow>> { get set }
-    
-    var shows: [TVShow] { get set }
-    
-    var cellsmodels: [TVShowCellViewModel] { get set }
-    
-    var showsLoadTask: Cancellable? { get set }
-    
-    func createModels(for fetched: [TVShow])
+  
+  associatedtype TVShowCellViewModel
+  
+  var fetchTVShowsUseCase: FetchTVShowsUseCase { get set }
+  
+  var filter: TVShowsListFilter { get set }
+  
+  var viewState: Observable<SimpleViewState<TVShow>> { get set }
+  
+  var shows: [TVShow] { get set }
+  
+  var cellsmodels: [TVShowCellViewModel] { get set }
+  
+  var showsLoadTask: Cancellable? { get set }
+  
+  func createModels(for fetched: [TVShow])
 }
 
 extension ShowsViewModel {
+  
+  func getShows(for page: Int) {
     
-    func getShows(for page: Int) {
-        
-        if viewState.value.isInitialPage {
-            viewState.value =  .loading
-        }
-        
-        let request = FetchTVShowsUseCaseRequestValue(filter: filter, page: page)
-        
-        showsLoadTask = fetchTVShowsUseCase.execute(requestValue: request) { [weak self] result in
-            guard let strongSelf = self else { return }
-            switch result {
-            case .success(let results):
-                strongSelf.processFetched(for: results)
-            case .failure(let error):
-                strongSelf.viewState.value = .error(error.localizedDescription)
-            }
-        }
+    if viewState.value.isInitialPage {
+      viewState.value =  .loading
     }
     
-    private func processFetched(for response: TVShowResult) {
-        let fetchedShows = response.results ?? []
-        
-        self.shows.append(contentsOf: fetchedShows)
-        
-        if self.shows.isEmpty {
-            viewState.value = .empty
-            return
-        }
-        
-        if response.hasMorePages {
-            self.viewState.value = .paging(shows, next: response.nextPage)
-        } else {
-            self.viewState.value = .populated(shows)
-        }
-        
-        self.createModels(for: fetchedShows)
+    let request = FetchTVShowsUseCaseRequestValue(filter: filter, page: page)
+    
+    showsLoadTask = fetchTVShowsUseCase.execute(requestValue: request) { [weak self] result in
+      guard let strongSelf = self else { return }
+      switch result {
+      case .success(let results):
+        strongSelf.processFetched(for: results)
+      case .failure(let error):
+        strongSelf.viewState.value = .error(error.localizedDescription)
+      }
     }
+  }
+  
+  private func processFetched(for response: TVShowResult) {
+    let fetchedShows = response.results ?? []
+    
+    self.shows.append(contentsOf: fetchedShows)
+    
+    self.createModels(for: fetchedShows)
+    
+    if self.shows.isEmpty ||
+      (fetchedShows.isEmpty && response.page == 1) {
+      viewState.value = .empty
+      return
+    }
+    
+    if response.hasMorePages {
+      self.viewState.value = .paging(shows, next: response.nextPage)
+    } else {
+      self.viewState.value = .populated(shows)
+    }
+  }
 }

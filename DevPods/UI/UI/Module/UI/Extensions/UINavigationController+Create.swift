3de84fba71f45545532a73efd16ef06edd733ea6
Jeans Ruiz <jeansruiz.c@gmail.com>
2020-07-15 15:53:39 -0500

    Fix color and font of UINavigationBar in iOS 13

--- DevPods/UI/UI/Module/UI/Extensions/UINavigationController+Create.swift
+++ DevPods/UI/UI/Module/UI/Extensions/UINavigationController+Create.swift
@@ -11,13 +11,37 @@ public extension UINavigationController {
   
   class func replaceAppearance() {
     
-    UINavigationBar.appearance().titleTextAttributes = [
-      NSAttributedString.Key.font: Font.montserrat.of(type: .bold, with: .normal),
-      NSAttributedString.Key.foregroundColor: Colors.electricBlue.color
-    ]
-    
-    UIBarButtonItem.appearance().setTitleTextAttributes([
-      NSAttributedString.Key.font: Font.montserrat.of(type: .bold, with: .normal)
-    ], for: .normal)
+    if #available(iOS 13.0, * ) {
+      let standard = UINavigationBarAppearance()
+      standard.configureWithTransparentBackground()
+      
+      standard.titleTextAttributes = [
+        .foregroundColor: Colors.electricBlue.color,
+        .font: Font.montserrat.of(type: .bold, with: .normal)]
+      
+      let button = UIBarButtonItemAppearance(style: .plain)
+      button.normal.titleTextAttributes = [
+        NSAttributedString.Key.foregroundColor: Colors.electricBlue.color,
+        .font: Font.montserrat.of(type: .bold, with: .normal)]
+      standard.buttonAppearance = button
+      
+      let done = UIBarButtonItemAppearance(style: .done)
+      done.normal.titleTextAttributes = [
+        NSAttributedString.Key.foregroundColor: Colors.electricBlue.color,
+        .font: Font.montserrat.of(type: .bold, with: .normal)]
+      standard.doneButtonAppearance = done
+      
+      UINavigationBar.appearance().standardAppearance = standard
+      
+    } else {
+      UINavigationBar.appearance().titleTextAttributes = [
+        NSAttributedString.Key.font: Font.montserrat.of(type: .bold, with: .normal),
+        NSAttributedString.Key.foregroundColor: Colors.electricBlue.color
+      ]
+      
+      UIBarButtonItem.appearance().setTitleTextAttributes([
+        NSAttributedString.Key.font: Font.montserrat.of(type: .bold, with: .normal)
+      ], for: .normal)
+    }
   }
 }

db729cfb41439b42cd48760509a585ffb4071868
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-09-23 18:09:34 -0500

    Add reference to resources in UI

--- Projects/Features/UI/Project.swift
+++ Projects/Features/UI/Project.swift
@@ -1,4 +1,7 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let project = Project.framework(name: "UI")
+let project = Project.framework(name: "UI",
+                                resources: [
+                                  "Resources/**"
+                                ])

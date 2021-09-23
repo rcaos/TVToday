dbf79558f15893d31c358f06beec1f11af487f1a
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-09-23 17:34:07 -0500

    Add initial Project and workspace files

---
+++ Projects/TVToday/Project.swift
@@ -0,0 +1,36 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.app(
+  name: "TVToday",
+  packages: [
+//    .remote(
+//      url: "https://github.com/ReactiveX/RxSwift.git",
+//      requirement: .upToNextMajor(from: "6.2.0")
+//    ),
+//    .remote(
+//      url: "https://github.com/RxSwiftCommunity/RxDataSources.git",
+//      requirement: .upToNextMajor(from: "5.0.0")
+//    )
+  ],
+  resources: [
+    "Resources/**"
+  ],
+  features: [
+//    "Networking",
+//    "Shared",
+//    "SosafeUI",
+//    "Feed"
+  ],
+  
+  dependencies: [
+//    .project(
+//      target: "SosafeReactiveKit",
+//      path: .relativeToRoot("Projects/SosafeReactiveKit")
+//    ),
+//    .project(
+//      target: "SosafeReactiveKitUI",
+//      path: .relativeToRoot("Projects/SosafeReactiveKitUI")
+//    )
+  ]
+)
---
+++ Workspace.swift
@@ -0,0 +1,9 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let workspace = Workspace(name: "TVToday", projects: [
+  "Projects/TVToday",
+//  "Projects/ProjectExamples/FeedExample",
+//  "Projects/SosafeReactiveKit",
+//  "Projects/SosafeReactiveKitUI"
+])

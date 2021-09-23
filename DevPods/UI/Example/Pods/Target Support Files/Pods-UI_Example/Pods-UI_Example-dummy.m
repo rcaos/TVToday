573483318e7b0900682dc1b20d501df3abf16fcf
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-09-23 17:10:24 -0500

    Remove pods references

---
+++ DevPods/UI/Example/Pods/Target Support Files/Pods-UI_Example/Pods-UI_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UI_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UI_Example
+@end
---
+++ DevPods/UI/Example/Pods/Target Support Files/UI/UI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UI : NSObject
+@end
+@implementation PodsDummy_UI
+@end

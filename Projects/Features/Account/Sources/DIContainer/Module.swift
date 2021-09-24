9163df0f83a14d8e465a2e48c0d1c2a45908c69e
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-09-24 09:07:00 -0500

    Move accounto into framework

---
+++ Projects/Features/Account/Sources/DIContainer/Module.swift
@@ -0,0 +1,42 @@
+//
+//  AccountDependencies.swift
+//  Account
+//
+//  Created by Jeans Ruiz on 6/26/20.
+//
+
+import Foundation
+import UIKit
+import Networking
+import Persistence
+import Shared
+
+public struct ModuleDependencies {
+  
+  let apiDataTransferService: DataTransferService
+  let imagesBaseURL: String
+  let showsPersistence: ShowsVisitedLocalRepository
+  
+  public init(apiDataTransferService: DataTransferService,
+              imagesBaseURL: String,
+              showsPersistence: ShowsVisitedLocalRepository) {
+    self.apiDataTransferService = apiDataTransferService
+    self.imagesBaseURL = imagesBaseURL
+    self.showsPersistence = showsPersistence
+  }
+}
+
+// MARK: - Entry to Module
+
+public struct Module {
+  
+  private let diContainer: DIContainer
+  
+  public init(dependencies: ModuleDependencies) {
+    self.diContainer = DIContainer(dependencies: dependencies)
+  }
+  
+  public func buildAccountCoordinator(in navigationController: UINavigationController) -> Coordinator {
+    return diContainer.buildModuleCoordinator(navigationController: navigationController)
+  }
+}

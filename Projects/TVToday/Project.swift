e29faf677796dd63b06fc29f84b59cd39fe54a13
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-09-24 06:46:31 -0500

    Add rxswift and rxdatasources as dependencies

--- Projects/TVToday/Project.swift
+++ Projects/TVToday/Project.swift
@@ -4,14 +4,14 @@ import ProjectDescriptionHelpers
 let project = Project.app(
   name: "TVToday",
   packages: [
-    //    .remote(
-    //      url: "https://github.com/ReactiveX/RxSwift.git",
-    //      requirement: .upToNextMajor(from: "6.2.0")
-    //    ),
-    //    .remote(
-    //      url: "https://github.com/RxSwiftCommunity/RxDataSources.git",
-    //      requirement: .upToNextMajor(from: "5.0.0")
-    //    )
+    .remote(
+      url: "https://github.com/ReactiveX/RxSwift.git",
+      requirement: .upToNextMajor(from: "6.2.0")
+    ),
+    .remote(
+      url: "https://github.com/RxSwiftCommunity/RxDataSources.git",
+      requirement: .upToNextMajor(from: "5.0.0")
+    ),
     .remote(
       url: "https://github.com/evgenyneu/keychain-swift.git",
       requirement: .upToNextMajor(from: "14.0.0")
@@ -23,18 +23,17 @@ let project = Project.app(
   features: [
     "UI",
     "KeyChainStorage",
-    "Persistence",
-    //    "Feed"
+    "Persistence"
   ],
   
   dependencies: [
-    //    .project(
-    //      target: "SosafeReactiveKit",
-    //      path: .relativeToRoot("Projects/SosafeReactiveKit")
-    //    ),
-    //    .project(
-    //      target: "SosafeReactiveKitUI",
-    //      path: .relativeToRoot("Projects/SosafeReactiveKitUI")
-    //    )
+    .project(
+      target: "ReactiveKit",
+      path: .relativeToRoot("Projects/ReactiveKit")
+    ),
+    .project(
+      target: "ReactiveKitUI",
+      path: .relativeToRoot("Projects/ReactiveKitUI")
+    )
   ]
 )
--- Workspace.swift
+++ Workspace.swift
@@ -4,6 +4,6 @@ import ProjectDescriptionHelpers
 let workspace = Workspace(name: "TVToday", projects: [
   "Projects/TVToday",
 //  "Projects/ProjectExamples/FeedExample",
-//  "Projects/SosafeReactiveKit",
-//  "Projects/SosafeReactiveKitUI"
+  "Projects/ReactiveKit",
+  "Projects/ReactiveKitUI"
 ])

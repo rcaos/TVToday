8897d709089b93012d2f07b4057beb70c4e27b8c
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-09-24 20:38:47 -0500

    Set fix app configuration

--- Projects/TVToday/Sources/AppConfigurations.swift
+++ Projects/TVToday/Sources/AppConfigurations.swift
@@ -13,23 +13,26 @@ import Foundation
 final class AppConfigurations {
   
   lazy var apiKey: String = {
-    guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
-      fatalError("ApiKey must not be empty in plist")
-    }
-    return apiKey
+    return "06e1a8c1f39b7a033e2efb972625fee2"
+//    guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+//      fatalError("ApiKey must not be empty in plist")
+//    }
+//    return apiKey
   }()
-  
+
   lazy var apiBaseURL: String = {
-    guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
-      fatalError("ApiBaseURL must not be empty in plist")
-    }
-    return apiBaseURL
+    return "https://api.themoviedb.org"
+//    guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+//      fatalError("ApiBaseURL must not be empty in plist")
+//    }
+//    return apiBaseURL
   }()
   
   lazy var imagesBaseURL: String = {
-    guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "ImageBaseURL") as? String else {
-      fatalError("ApiBaseURL must not be empty in plist")
-    }
-    return imageBaseURL
+    return "https://image.tmdb.org"
+//    guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "ImageBaseURL") as? String else {
+//      fatalError("ApiBaseURL must not be empty in plist")
+//    }
+//    return imageBaseURL
   }()
 }

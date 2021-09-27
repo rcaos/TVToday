9d143df1a1ae980179a1f578c416d6b4d312ab3e
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-09-27 09:18:54 -0500

    Remove comments

--- Projects/Features/ShowDetails/Project.swift
+++ Projects/Features/ShowDetails/Project.swift
@@ -9,18 +9,6 @@ let project = Project.framework(
       target: "UI",
       path: .relativeToRoot("Projects/Features/UI")
     ),
-//    .project(
-//      target: "Networking",
-//      path: .relativeToRoot("Projects/Features/Networking")
-//    ),
-//    .project(
-//      target: "Shared",
-//      path: .relativeToRoot("Projects/Features/Shared")
-//    ),
-//    .project(
-//      target: "Persistence",
-//      path: .relativeToRoot("Projects/Features/Persistence")
-//    ),
     .package(product: "RxCocoa"),
     .package(product: "RxDataSources"),
     .package(product: "RxSwift"),

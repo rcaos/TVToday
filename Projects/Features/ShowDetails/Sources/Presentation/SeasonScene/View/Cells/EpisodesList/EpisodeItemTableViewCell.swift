cf869b6a377ed07ac26ef490c59c7ab2281e72a3
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-10-04 13:58:44 -0500

    Add constraints in code

--- Projects/Features/ShowDetails/Sources/Presentation/SeasonScene/View/Cells/EpisodesList/EpisodeItemTableViewCell.swift
+++ Projects/Features/ShowDetails/Sources/Presentation/SeasonScene/View/Cells/EpisodesList/EpisodeItemTableViewCell.swift
@@ -10,42 +10,129 @@ import UIKit
 import Shared
 import UI
 
-class EpisodeItemTableViewCell: UITableViewCell {
+class EpisodeItemTableViewCell: NiblessTableViewCell {
 
-  @IBOutlet private weak var episodeImage: UIImageView!
-  @IBOutlet private weak var episodeNumberLabel: TVRegularLabel!
-  @IBOutlet private weak var episodeNameLabel: TVRegularLabel!
-  @IBOutlet private weak var releaseLabel: TVRegularLabel!
-  @IBOutlet private weak var durationLabel: TVRegularLabel!
-  @IBOutlet private weak var starImageView: UIImageView!
-  @IBOutlet private weak var averageLabel: TVRegularLabel!
+  private let separatorView = UIView()
 
-  var viewModel: EpisodeItemViewModel? {
-    didSet {
-      setupUI()
-    }
+  private let episodeImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(name: "placeholder")
+    imageView.contentMode = .scaleAspectFill
+    imageView.clipsToBounds = true
+    return imageView
+  }()
+
+  private let episodeNameLabel = TVRegularLabel(frame: .zero)
+
+  private let releaseLabel = TVRegularLabel(frame: .zero)
+
+  private lazy var rightContainerStackView: UIStackView = {
+    let stack = UIStackView(arrangedSubviews: [episodeNameLabel, releaseLabel, averageStackView])
+    stack.axis = .vertical
+    stack.alignment = .leading
+    stack.distribution = .fill
+    stack.spacing = 8.0
+
+    stack.setContentCompressionResistancePriority(.required, for: .horizontal)
+
+    return stack
+  }()
+
+  private let starImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(name: "star")
+    imageView.contentMode = .scaleAspectFit
+
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      imageView.widthAnchor.constraint(equalToConstant: 20),
+      imageView.heightAnchor.constraint(equalToConstant: 20)
+    ])
+    return imageView
+  }()
+
+  private let averageLabel = TVRegularLabel(frame: .zero)
+
+  private lazy var averageStackView: UIStackView = {
+    let stack = UIStackView(arrangedSubviews: [starImageView, averageLabel])
+    stack.axis = .horizontal
+    stack.alignment = .fill
+    stack.distribution = .fill
+    stack.spacing = 5.0
+    return stack
+  }()
+
+  private var viewModel: EpisodeItemViewModel?
+
+  public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setupUI()
   }
 
-  override func awakeFromNib() {
-    super.awakeFromNib()
-    setupElements()
-  }
+  func setModel(viewModel: EpisodeItemViewModel) {
+    self.viewModel = viewModel
 
-  private func setupElements() {
-    starImageView.image = UIImage(name: "star")
+    let episodeNumber = viewModel.episodeNumber ?? ""
+    let episodeName = viewModel.episodeName ?? ""
+    episodeNameLabel.text = "\(episodeNumber) \(episodeName)"
+    releaseLabel.text = viewModel.releaseDate
+    averageLabel.text = viewModel.average
+    episodeImageView.setImage(with: viewModel.posterURL, placeholder: UIImage(name: "placeholder"))
   }
 
   private func setupUI() {
-    durationLabel.text = ""
+    constructHierarchy()
+    activateConstraints()
+    setupViews()
+  }
 
-    episodeNumberLabel.text = viewModel?.episodeNumber
-    episodeNameLabel.text = viewModel?.episodeName
-    releaseLabel.text = viewModel?.releaseDate
-    averageLabel.text = viewModel?.average
-    episodeImage.setImage(with: viewModel?.posterURL, placeholder: UIImage(name: "placeholder"))
+  private func setupViews() {
+    separatorView.backgroundColor = .lightGray
+    separatorView.alpha = 0.2
+  }
+
+  private func constructHierarchy() {
+    contentView.addSubview(separatorView)
+    contentView.addSubview(episodeImageView)
+    contentView.addSubview(rightContainerStackView)
+  }
+
+  private func activateConstraints() {
+    activateConstraintsForSeparatorView()
+    activateConstraintsForPosterView()
+    activateConstraintsForLeftStackView()
+  }
+
+  private func activateConstraintsForSeparatorView() {
+    separatorView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      separatorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+      separatorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+      separatorView.topAnchor.constraint(equalTo: contentView.topAnchor),
+      separatorView.heightAnchor.constraint(equalToConstant: 1.0)
+    ])
+  }
+
+  private func activateConstraintsForPosterView() {
+    episodeImageView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      episodeImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+      episodeImageView.trailingAnchor.constraint(equalTo: rightContainerStackView.leadingAnchor, constant: -8),
+      episodeImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.25),
+      episodeImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+      episodeImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+    ])
+  }
+
+  private func activateConstraintsForLeftStackView() {
+    rightContainerStackView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      rightContainerStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+      rightContainerStackView.centerYAnchor.constraint(equalTo: centerYAnchor)
+    ])
   }
 
   override func prepareForReuse() {
-    episodeImage.image = nil
+    episodeImageView.image = nil
   }
 }

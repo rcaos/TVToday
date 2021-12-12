cccf5046abf7493c1b08327cb0df57cec169d6ba
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-12 00:23:33 -0500

    Remove unused code

--- Projects/Features/UI/Sources/Extensions/UINavigationController+Create.swift
+++ Projects/Features/UI/Sources/Extensions/UINavigationController+Create.swift
@@ -10,36 +10,25 @@ import UIKit
 public extension UINavigationController {
 
   class func replaceAppearance() {
-    if #available(iOS 13.0, * ) {
-      let standard = UINavigationBarAppearance()
-      standard.configureWithDefaultBackground()
+    let standard = UINavigationBarAppearance()
+    standard.configureWithDefaultBackground()
 
-      standard.titleTextAttributes = [
-        .foregroundColor: Colors.electricBlue.color,
-        .font: Font.montserrat.of(type: .bold, with: .normal)]
+    standard.titleTextAttributes = [
+      .foregroundColor: Colors.electricBlue.color,
+      .font: Font.montserrat.of(type: .bold, with: .normal)]
 
-      let button = UIBarButtonItemAppearance(style: .plain)
-      button.normal.titleTextAttributes = [
-        NSAttributedString.Key.foregroundColor: Colors.electricBlue.color,
-        .font: Font.montserrat.of(type: .bold, with: .normal)]
-      standard.buttonAppearance = button
+    let button = UIBarButtonItemAppearance(style: .plain)
+    button.normal.titleTextAttributes = [
+      NSAttributedString.Key.foregroundColor: Colors.electricBlue.color,
+      .font: Font.montserrat.of(type: .bold, with: .normal)]
+    standard.buttonAppearance = button
 
-      let done = UIBarButtonItemAppearance(style: .done)
-      done.normal.titleTextAttributes = [
-        NSAttributedString.Key.foregroundColor: Colors.electricBlue.color,
-        .font: Font.montserrat.of(type: .bold, with: .normal)]
-      standard.doneButtonAppearance = done
+    let done = UIBarButtonItemAppearance(style: .done)
+    done.normal.titleTextAttributes = [
+      NSAttributedString.Key.foregroundColor: Colors.electricBlue.color,
+      .font: Font.montserrat.of(type: .bold, with: .normal)]
+    standard.doneButtonAppearance = done
 
-      UINavigationBar.appearance().standardAppearance = standard
-    } else {
-      UINavigationBar.appearance().titleTextAttributes = [
-        NSAttributedString.Key.font: Font.montserrat.of(type: .bold, with: .normal),
-        NSAttributedString.Key.foregroundColor: Colors.electricBlue.color
-      ]
-
-      UIBarButtonItem.appearance().setTitleTextAttributes([
-        NSAttributedString.Key.font: Font.montserrat.of(type: .bold, with: .normal)
-      ], for: .normal)
-    }
+    UINavigationBar.appearance().standardAppearance = standard
   }
 }

8f6aba6acbeaa69095501c33872253051ff37590
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-12 02:54:46 -0500

    Change background color in Popular view

--- Projects/Features/Shared/Sources/Common/Cells/TVShowViewCell.swift
+++ Projects/Features/Shared/Sources/Common/Cells/TVShowViewCell.swift
@@ -63,6 +63,7 @@ public class TVShowViewCell: NiblessTableViewCell {
   }
 
   private func setupUI() {
+    backgroundColor = .secondarySystemBackground
     constructHierarchy()
     activateConstraints()
   }

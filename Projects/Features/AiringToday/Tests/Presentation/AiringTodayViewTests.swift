25cebad62931cb6e3d8deb764fd5d43889587ce7
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-19 15:25:09 -0500

    Delete references to FBSnapshot test library

--- Projects/Features/AiringToday/Tests/Presentation/AiringTodayViewTests.swift
+++ Projects/Features/AiringToday/Tests/Presentation/AiringTodayViewTests.swift
@@ -7,114 +7,81 @@
 
 // swiftlint:disable all
 
-//import FBSnapshotTestCase
-//import RxSwift
-//
-//@testable import AiringToday
-//@testable import Shared
-//
-//class AiringTodayViewTests: FBSnapshotTestCase {
-//  
-//  let firstShow = TVShow.stub(id: 1, name: "title1 🐶", posterPath: "/1",
-//                              backDropPath: "/back1", overview: "overview")
-//  let secondShow = TVShow.stub(id: 2, name: "title2 🔫", posterPath: "/2",
-//                               backDropPath: "/back2", overview: "overview2")
-//  let thirdShow = TVShow.stub(id: 3, name: "title3 🚨", posterPath: "/3",
-//                              backDropPath: "/back3", overview: "overview3")
-//  
-//  lazy var firstPage = TVShowResult.stub(page: 1,
-//                                         results: [firstShow, secondShow],
-//                                         totalResults: 3,
-//                                         totalPages: 2)
-//  
-//  lazy var secondPage = TVShowResult.stub(page: 2,
-//                                          results: [thirdShow],
-//                                          totalResults: 3,
-//                                          totalPages: 2)
-//  
-//  private var rootWindow: UIWindow!
-//  
-//  override func setUp() {
-//    super.setUp()
-//    //self.recordMode = true
-//    rootWindow = UIWindow(frame: UIScreen.main.bounds)
-//    rootWindow.isHidden = false
-//  }
-//  
-//  func test_WhenViewIsLoading_thenShowLoadingScreen() {
-//    // given
-//    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .loading) )
-//    rootWindow.rootViewController = viewController
-//    
-//    FBSnapshotVerifyView(viewController.view)
-//  }
-//  
-//  func test_WhenViewPaging_thenShowPagingScreen() {
-//    
-//    let firsPageCells = firstPage.results!.map { AiringTodayCollectionViewModel(show: $0) }
-//    
-//    // given
-//    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .paging(firsPageCells, next: 2) ) )
-//    rootWindow.rootViewController = viewController
-//    
-//    FBSnapshotVerifyView(viewController.view)
-//  }
-//  
-//  func test_WhenViewPopulated_thenShowPopulatedScreen() {
-//    
-//    let totalCells = (self.firstPage.results + self.secondPage.results)
-//               .map { AiringTodayCollectionViewModel(show: $0) }
-//    
-//    // given
-//    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .populated(totalCells) ))
-//    rootWindow.rootViewController = viewController
-//    
-//    FBSnapshotVerifyView(viewController.view)
-//  }
-//  
-//  func test_WhenViewIsEmpty_thenShowEmptyScreen() {
-//    // given
-//    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .empty ) )
-//    rootWindow.rootViewController = viewController
-//    
-//    FBSnapshotVerifyView(viewController.view)
-//  }
-//  
-//  func test_WhenViewIsError_thenShowErrorScreen() {
-//    // given
-//    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .error("Error to Fetch Shows") ) )
-//    rootWindow.rootViewController = viewController
-//    
-//    FBSnapshotVerifyView(viewController.view)
-//  }
-//  
-//}
-//
-//// MARK: AiringTodayViewModelProtocol
-//
-//class AiringTodayViewModelMock: AiringTodayViewModelProtocol {
-//  
-//  func viewDidLoad() { }
-//  
-//  func didLoadNextPage() { }
-//  
-//  func showIsPicked(with id: Int) { }
-//  
-//  func refreshView() { }
-//  
-//  func getCurrentViewState() -> SimpleViewState<AiringTodayCollectionViewModel> {
-//    if let currentViewState = try? viewStateObservableSubject.value() {
-//      return currentViewState
-//    }
-//    return .empty
-//  }
-//  
-//  var viewState: Observable<SimpleViewState<AiringTodayCollectionViewModel>>
-//  
-//  var viewStateObservableSubject: BehaviorSubject<SimpleViewState<AiringTodayCollectionViewModel>>
-//  
-//  init(state: SimpleViewState<AiringTodayCollectionViewModel>) {
-//    viewStateObservableSubject = BehaviorSubject(value: state)
-//    viewState = viewStateObservableSubject.asObservable()
-//  }
-//}
+import XCTest
+import SnapshotTesting
+import RxSwift
+
+@testable import AiringToday
+@testable import Shared
+
+class AiringTodayViewTests: XCTestCase {
+
+  let firstShow = TVShow.stub(id: 1, name: "title1 🐶", posterPath: "/1",
+                              backDropPath: "/back1", overview: "overview")
+  let secondShow = TVShow.stub(id: 2, name: "title2 🔫", posterPath: "/2",
+                               backDropPath: "/back2", overview: "overview2")
+  let thirdShow = TVShow.stub(id: 3, name: "title3 🚨", posterPath: "/3",
+                              backDropPath: "/back3", overview: "overview3")
+
+  lazy var firstPage = TVShowResult.stub(page: 1,
+                                         results: [firstShow, secondShow],
+                                         totalResults: 3,
+                                         totalPages: 2)
+
+  lazy var secondPage = TVShowResult.stub(page: 2,
+                                          results: [thirdShow],
+                                          totalResults: 3,
+                                          totalPages: 2)
+
+  override func setUp() {
+    super.setUp()
+    isRecording = false
+  }
+
+  func test_WhenViewIsLoading_thenShowLoadingScreen() {
+    // when
+    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .loading) )
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+
+  func test_WhenViewPaging_thenShowPagingScreen() {
+    // given
+    let firsPageCells = firstPage.results!.map { AiringTodayCollectionViewModel(show: $0) }
+
+    // when
+    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .paging(firsPageCells, next: 2) ) )
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+
+  func test_WhenViewPopulated_thenShowPopulatedScreen() {
+    // given
+    let totalCells = (self.firstPage.results + self.secondPage.results)
+      .map { AiringTodayCollectionViewModel(show: $0) }
+
+    // when
+    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .populated(totalCells) ))
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+
+  func test_WhenViewIsEmpty_thenShowEmptyScreen() {
+    // when
+    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .empty ) )
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+
+  func test_WhenViewIsError_thenShowErrorScreen() {
+    // when
+    let viewController = AiringTodayViewController(viewModel: AiringTodayViewModelMock(state: .error("Error to Fetch Shows") ) )
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+}

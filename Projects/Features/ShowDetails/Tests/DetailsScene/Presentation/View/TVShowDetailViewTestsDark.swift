f78b7701e9130b5f187091a986e4f3dd713c7705
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-19 16:27:42 -0500

    Support dark tests

---
+++ Projects/Features/ShowDetails/Tests/DetailsScene/Presentation/View/TVShowDetailViewTestsDark.swift
@@ -0,0 +1,57 @@
+//
+//  TVShowDetailViewTestsDark.swift
+//  ShowDetailsTests
+//
+//  Created by Jeans Ruiz on 19/12/21.
+//
+
+import SnapshotTesting
+import RxSwift
+
+@testable import ShowDetails
+@testable import Shared
+
+class TVShowDetailViewTestsLight: FBSnapshotTestCase {
+
+  override func setUp() {
+    super.setUp()
+    isRecording = false
+  }
+
+  func test_WhenViewIsLoading_thenShowLoadingScreen() {
+    // given
+    let initialState = TVShowDetailViewModelMock(state: .loading)
+
+    // when
+    let viewController = TVShowDetailViewController(viewModel: initialState)
+    viewController.overrideUserInterfaceStyle = .dark
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+
+  func test_WhenViewPopulated_thenShowPopulatedScreen() {
+    // given
+    let initialState = TVShowDetailViewModelMock(state: .populated( TVShowDetailInfo.stub() ))
+
+    // when
+    let viewController = TVShowDetailViewController(viewModel: initialState)
+    viewController.overrideUserInterfaceStyle = .dark
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
+  }
+
+  func test_WhenViewIsError_thenShowErrorScreen() {
+    // given
+    let initialState = TVShowDetailViewModelMock(state: .error("Error to Fetch Details"))
+
+    // when
+    let viewController = TVShowDetailViewController(viewModel: initialState)
+    viewController.overrideUserInterfaceStyle = .dark
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+}

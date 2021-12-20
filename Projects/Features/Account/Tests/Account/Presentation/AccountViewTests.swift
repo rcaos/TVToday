6e43515aac70db1ab7faeb462cd603eab440a60b
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-20 09:11:32 -0500

    Remove references to old framework

--- Projects/Features/Account/Tests/Account/Presentation/AccountViewTests.swift
+++ Projects/Features/Account/Tests/Account/Presentation/AccountViewTests.swift
@@ -1,66 +1,45 @@
 //
-//  AccountViewTests.swift
+//  AccountViewLightTests.swift
 //  AccountTV
 //
 //  Created by Jeans Ruiz on 6/19/20.
 //  Copyright © 2020 Jeans. All rights reserved.
 //
 
-// swiftlint:disable all
+import XCTest
+import SnapshotTesting
+import RxSwift
 
-// MARK: - TODO, change FBSnapshotTestCase
+@testable import Account
 
-//import FBSnapshotTestCase
-//import RxSwift
-//
-//@testable import Account
-//
-//class AccountViewTests: FBSnapshotTestCase {
-//
-//  private var rootWindow: UIWindow!
-//
-//  override func setUp() {
-//    super.setUp()
-//    //self.recordMode = true
-//    rootWindow = UIWindow(frame: UIScreen.main.bounds)
-//    rootWindow.isHidden = false
-//  }
-//
-//  func test_WhenViewIsLogin_thenShowLoginScreen() {
-//    // given
-//    let accountViewModel = AccountViewModelMock(state: .login)
-//
-//    let factory = AccountViewControllerFactoryMock()
-//
-//    let viewController = AccountViewController(viewModel: accountViewModel, viewControllersFactory: factory)
-//    rootWindow.rootViewController = viewController
-//
-//    FBSnapshotVerifyView(viewController.view)
-//  }
-//
-//  func test_WhenViewIsLogged_thenShowProfileScreen() {
-//    // given
-//    let accountViewModel = AccountViewModelMock(state: .profile(account: AccountResult.stub()))
-//
-//    let factory = AccountViewControllerFactoryMock()
-//
-//    let viewController = AccountViewController(viewModel: accountViewModel, viewControllersFactory: factory)
-//    rootWindow.rootViewController = viewController
-//
-//    FBSnapshotVerifyView(viewController.view)
-//  }
-//}
-//
-//// MARK: - AccountViewControllerFactory
-//
-//class AccountViewControllerFactoryMock: AccountViewControllerFactory {
-//  func makeSignInViewController() -> UIViewController {
-//    let viewModel = SignInViewModelMock(state: .initial)
-//    return SignInViewController(viewModel: viewModel)
-//  }
-//
-//  func makeProfileViewController(with account: AccountResult) -> UIViewController {
-//    let viewModel =  ProfileViewModelMock(account: account)
-//    return ProfileViewController(viewModel: viewModel)
-//  }
-//}
+class AccountViewLightTests: XCTestCase {
+
+  private let factory = AccountViewControllerFactoryMock()
+
+  override func setUp() {
+    super.setUp()
+    isRecording = false
+  }
+
+  func test_WhenViewIsLogin_thenShowLoginScreen() {
+    // given
+    let accountViewModel = AccountViewModelMock(state: .login)
+
+    let viewController = AccountViewController(viewModel: accountViewModel, viewControllersFactory: factory)
+    viewController.overrideUserInterfaceStyle = .light
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+
+  func test_WhenViewIsLogged_thenShowProfileScreen() {
+    // given
+    let accountViewModel = AccountViewModelMock(state: .profile(account: AccountResult.stub()))
+
+    let viewController = AccountViewController(viewModel: accountViewModel, viewControllersFactory: factory)
+    viewController.overrideUserInterfaceStyle = .light
+
+    // then
+    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
+  }
+}

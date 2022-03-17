d157a33114994d376be1f829e36fbb282a70fe23
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-17 15:34:52 -0500

    Remove rx datsource model type

--- Projects/Features/SearchShows/Sources/Presentation/SearchOptions/Cells/VisitedShowTableViewCell/VisitedShowSectionModel.swift
+++ Projects/Features/SearchShows/Sources/Presentation/SearchOptions/Cells/VisitedShowTableViewCell/VisitedShowSectionModel.swift
@@ -5,19 +5,8 @@
 //  Created by Jeans Ruiz on 7/8/20.
 //
 
-import RxDataSources
 import Persistence
 
-struct VisitedShowSectionModel {
-  var header: String
-  var items: [Item]
-}
-
-extension VisitedShowSectionModel: SectionModelType {
-  typealias Item = ShowVisited
-
-  init(original: VisitedShowSectionModel, items: [Item]) {
-    self = original
-    self.items = items
-  }
+enum HeaderModel: Hashable {
+  case header
 }

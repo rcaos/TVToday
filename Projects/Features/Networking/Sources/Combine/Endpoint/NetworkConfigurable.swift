427627d6187f70022c920cf1865fe5b3e1d68c7f
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-19 17:50:13 -0500

    Move configurable file

--- Projects/Features/Networking/Sources/Combine/Endpoint/NetworkConfigurable.swift
+++ Projects/Features/Networking/Sources/Combine/Endpoint/NetworkConfigurable.swift
@@ -7,30 +7,17 @@
 //
 
 import Foundation
-
-public protocol NetworkConfigurable {
-
-  var baseURL: String { get }
-
-  /// URLRequest.allHTTPHeaderFields
-  var headers: [String: String] { get }
-
-  /// URLComponents.queryItems:
-  /// 1: Global query Parameters should be Here. For All requests
-  /// &api_key=XXX
-  /// &language=en
-  var queryParameters: [String: String] { get }
-}
+import NetworkingInterface
 
 public struct ApiDataNetworkConfig: NetworkConfigurable {
 
-  public let baseURL: String
+  public let baseURL: URL
 
   public let headers: [String: String]
 
   public let queryParameters: [String: String]
 
-  public init(baseURL: String,
+  public init(baseURL: URL,
               headers: [String: String] = [:],
               queryParameters: [String: String] = [:]) {
     self.baseURL = baseURL

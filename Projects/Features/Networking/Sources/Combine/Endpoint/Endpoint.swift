b10b0c78530374728ef05409a0ad11cc1535d4ba
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-19 17:47:38 -0500

    Add networking classes

---
+++ Projects/Features/Networking/Sources/Combine/Endpoint/Endpoint.swift
@@ -0,0 +1,111 @@
+//
+//  Endpoint.swift
+//  Networking
+//
+//  Created by Jeans Ruiz on 19/03/22.
+//
+
+import Foundation
+import NetworkingInterface
+
+public class Endpoint<R>: ResponseRequestable {
+
+  public typealias Response = R
+
+  public var path: String
+  public var isFullPath: Bool
+  public var method: HTTPMethodType
+  public var headerParamaters: [String: String]
+  public var queryParametersEncodable: Encodable?
+  public var queryParameters: [String: Any]
+  public var bodyParamatersEncodable: Encodable?
+  public var bodyParamaters: [String: Any]
+  public var bodyEncoding: BodyEncoding
+  public var responseDecoder: ResponseDecoder
+
+  public init(path: String,
+              isFullPath: Bool = false,
+              method: HTTPMethodType,
+              headerParamaters: [String: String] = [:],
+              queryParametersEncodable: Encodable? = nil,
+              queryParameters: [String: Any] = [:],
+              bodyParamatersEncodable: Encodable? = nil,
+              bodyParamaters: [String: Any] = [:],
+              bodyEncoding: BodyEncoding = .jsonSerializationData,
+              responseDecoder: ResponseDecoder = JSONResponseDecoder()) {
+    self.path = path
+    self.isFullPath = isFullPath
+    self.method = method
+    self.headerParamaters = headerParamaters
+    self.queryParametersEncodable = queryParametersEncodable
+    self.queryParameters = queryParameters
+    self.bodyParamatersEncodable = bodyParamatersEncodable
+    self.bodyParamaters = bodyParamaters
+    self.bodyEncoding = bodyEncoding
+    self.responseDecoder = responseDecoder
+  }
+
+  public func urlRequest(with config: NetworkConfigurable) throws -> URLRequest {
+
+    let url = try self.url(with: config)
+    var urlRequest = URLRequest(url: url)
+    var allHeaders: [String: String] = config.headers
+    headerParamaters.forEach { allHeaders.updateValue($1, forKey: $0) }
+
+    let bodyParamaters = try bodyParamatersEncodable?.toDictionary() ?? self.bodyParamaters
+    if !bodyParamaters.isEmpty {
+      urlRequest.httpBody = encodeBody(bodyParamaters: bodyParamaters, bodyEncoding: bodyEncoding)
+    }
+    urlRequest.httpMethod = method.rawValue
+    urlRequest.allHTTPHeaderFields = allHeaders
+    return urlRequest
+  }
+}
+
+extension Requestable {
+
+  func url(with config: NetworkConfigurable) throws -> URL {
+    let baseURL = config.baseURL.absoluteString.last != "/" ? config.baseURL.absoluteString + "/" : config.baseURL.absoluteString
+    let endpoint = isFullPath ? path : baseURL.appending(path)
+
+    guard var urlComponents = URLComponents(string: endpoint) else { throw RequestGenerationError.components }
+    var urlQueryItems = [URLQueryItem]()
+
+    let queryParameters = try queryParametersEncodable?.toDictionary() ?? self.queryParameters
+    queryParameters.forEach {
+      urlQueryItems.append(URLQueryItem(name: $0.key, value: "\($0.value)"))
+    }
+    config.queryParameters.forEach {
+      urlQueryItems.append(URLQueryItem(name: $0.key, value: $0.value))
+    }
+    urlComponents.queryItems = !urlQueryItems.isEmpty ? urlQueryItems : nil
+    guard let url = urlComponents.url else { throw RequestGenerationError.components }
+    return url
+  }
+
+  func encodeBody(bodyParamaters: [String: Any], bodyEncoding: BodyEncoding) -> Data? {
+    switch bodyEncoding {
+    case .jsonSerializationData:
+      return try? JSONSerialization.data(withJSONObject: bodyParamaters)
+    case .stringEncodingAscii:
+      return bodyParamaters.queryString.data(using: String.Encoding.ascii, allowLossyConversion: true)
+    }
+  }
+}
+
+private extension Dictionary {
+  var queryString: String {
+    return self.map { "\($0.key)=\($0.value)" }
+      .joined(separator: "&")
+      .addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) ?? ""
+  }
+}
+
+private extension Encodable {
+  func toDictionary() throws -> [String: Any]? {
+    let data = try JSONEncoder().encode(self)
+    let josnData = try JSONSerialization.jsonObject(with: data)
+    return josnData as? [String: Any]
+  }
+}
+

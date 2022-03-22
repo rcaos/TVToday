596dd9d4784edefdb7ccdf463ca15e1044cec3bf
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-21 20:46:26 -0500

    Change interface

--- Projects/Features/Account/Sources/Data/Repositories/DefaultAccountRepository.swift
+++ Projects/Features/Account/Sources/Data/Repositories/DefaultAccountRepository.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2020 Jeans. All rights reserved.
 //
 
-import RxSwift
+import Combine
 import NetworkingInterface
+import Networking
 import Shared
 
 public final class DefaultAccountRepository {
-
   private let dataTransferService: DataTransferService
 
   public init(dataTransferService: DataTransferService) {
@@ -21,8 +21,15 @@ public final class DefaultAccountRepository {
 
 // MARK: - AuthRepository
 extension DefaultAccountRepository: AccountRepository {
-  public func getAccountDetails(session: String) -> Observable<AccountResult> {
-    let endPoint = AccountProvider.accountDetails(sessionId: session)
-    return dataTransferService.request(endPoint, AccountResult.self)
+
+  public func getAccountDetails(session: String) -> AnyPublisher<AccountResult, DataTransferError> {
+    let endpoint = Endpoint<AccountResult>(
+      path: "3/account",
+      method: .get,
+      queryParameters: [
+        "session_id": session
+      ]
+    )
+    return dataTransferService.request(with: endpoint)
   }
 }

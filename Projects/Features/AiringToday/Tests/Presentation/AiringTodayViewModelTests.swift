41ebe06d76bf683170ca96f1e4533ca007fd12e0
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-29 17:49:57 -0500

    Add test for error state

--- Projects/Features/AiringToday/Tests/Presentation/AiringTodayViewModelTests.swift
+++ Projects/Features/AiringToday/Tests/Presentation/AiringTodayViewModelTests.swift
@@ -133,108 +133,43 @@ class AiringTodayViewModelTests: XCTestCase {
     XCTAssertEqual(expected, received, "Should contains 3 values")
   }
 
+  func test_When_UseCase_Responds_Error_ViewModel_Should_Contains_Error_State() {
+    // given
+    fetchUseCaseMock.error = .noResponse
+    let sut: AiringTodayViewModelProtocol
+    sut = AiringTodayViewModel(fetchTVShowsUseCase: self.fetchUseCaseMock, coordinator: nil)
+
+    // when
+    sut.viewDidLoad()
+
+    let expected = [
+      SimpleViewState<AiringTodayCollectionViewModel>.loading,
+      SimpleViewState<AiringTodayCollectionViewModel>.error("")
+    ]
+    var received = [SimpleViewState<AiringTodayCollectionViewModel>]()
+
+    sut.viewStateObservableSubject
+      .removeDuplicates()
+      .sink(receiveValue: { value in
+        received.append(value)
+
+      })
+      .store(in: &disposeBag)
+
+    _ = XCTWaiter.wait(for: [XCTestExpectation()], timeout: 0.1)
+
+    // then
+    XCTAssertEqual(2, received.count, "Should only receives one Value")
+    XCTAssertEqual(expected, received, "AiringTodayViewModel should contains Error State")
+  }
+
 //  override func spec() {
 //    describe("AiringTodayViewModel") {
 //      var fetchUseCaseMock: FetchShowsUseCaseMock!
 //      beforeEach {
 //        fetchUseCaseMock = FetchShowsUseCaseMock()
 //      }
-//
-//      context("When waiting for response of Fetch Use Case") {
-//        it("Should ViewModel contanins Loading State") {
-//          // given
-//          // not response yet
-//
-//          let viewModel: AiringTodayViewModelProtocol = AiringTodayViewModel(fetchTVShowsUseCase: fetchUseCaseMock, coordinator: nil)
-//
-//          // when
-//          viewModel.viewDidLoad()
-//
-//          // when
-//          let viewState = try? viewModel.viewState.toBlocking(timeout: 2).first()
-//          guard let currentViewState = viewState else {
-//            fail("It should emit a View State")
-//            return
-//          }
-//          let expected = SimpleViewState<AiringTodayCollectionViewModel>.loading
-//
-//          expect(currentViewState).toEventually(equal(expected))
-//        }
-//      }
 
-//      context("When Fetch Use Case Retrieve First page") {
-//        it("Should ViewModel contains only First page") {
-//          // given
-//          fetchUseCaseMock.result = self.firstPage
-//          let firstPageCells = self.firstPage.results!.map { AiringTodayCollectionViewModel(show: $0) }
-//
-//          let viewModel: AiringTodayViewModelProtocol = AiringTodayViewModel(fetchTVShowsUseCase: fetchUseCaseMock, coordinator: nil)
-//
-//          // when
-//          viewModel.viewDidLoad()
-//
-//          // then
-//          let viewState = try? viewModel.viewState.toBlocking(timeout: 2).first()
-//          guard let currentViewState = viewState else {
-//            fail("It should emit a View State")
-//            return
-//          }
-//          let expected = SimpleViewState<AiringTodayCollectionViewModel>.paging(firstPageCells, next: 2)
-//
-//          expect(currentViewState).toEventually(equal(expected))
-//        }
-//      }
-//
-//      context("When Fetch Use Case Retrieve First and Second Page") {
-//        it("Should ViewModel contains First and Second page") {
-//
-//          let totalCells = (self.firstPage.results + self.secondPage.results)
-//            .map { AiringTodayCollectionViewModel(show: $0) }
-//
-//          // given
-//          fetchUseCaseMock.result = self.firstPage
-//
-//          let viewModel: AiringTodayViewModelProtocol = AiringTodayViewModel(fetchTVShowsUseCase: fetchUseCaseMock, coordinator: nil)
-//
-//          // when
-//          viewModel.viewDidLoad()
-//          fetchUseCaseMock.result = self.secondPage
-//          viewModel.didLoadNextPage()
-//
-//          // then
-//          let viewState = try? viewModel.viewState.toBlocking(timeout: 2).first()
-//          guard let currentViewState = viewState else {
-//            fail("It should emit a View State")
-//            return
-//          }
-//          let expected = SimpleViewState<AiringTodayCollectionViewModel>.populated(totalCells)
-//
-//          expect(currentViewState).toEventually(equal(expected))
-//        }
-//      }
-//
-//      context("When Fetch Use Case Returns Error") {
-//        it("Should ViewModel contanins Error") {
-//          // given
-//          fetchUseCaseMock.error = CustomError.genericError
-//
-//          let viewModel = AiringTodayViewModel(fetchTVShowsUseCase: fetchUseCaseMock, coordinator: nil)
-//
-//          // when
-//          viewModel.viewDidLoad()
-//
-//          // when
-//          let viewState = try? viewModel.viewState.toBlocking(timeout: 2).first()
-//          guard let currentViewState = viewState else {
-//            fail("It should emit a View State")
-//            return
-//          }
-//          let expected = SimpleViewState<AiringTodayCollectionViewModel>.error("")
-//
-//          expect(currentViewState).toEventually(equal(expected))
-//        }
-//      }
-//
 //      context("When Fetch Use Case Returns Zero Shows") {
 //        it("Should ViewModel contanins Empty State") {
 //          // given

cc7d1015bf7323141b5daae59f9745a2fe892583
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-30 08:49:07 -0500

    Create build helper models

---
+++ Projects/Features/SearchShows/Tests/SearchOptions/Mocks/Entities/ShowVisited+Build.swift
@@ -0,0 +1,24 @@
+//
+//  ShowVisited+Build.swift
+//  SearchShowsTests
+//
+//  Created by Jeans Ruiz on 30/03/22.
+//
+
+import Persistence
+import Shared
+
+func buildShowVisited() -> [ShowVisited] {
+  return [
+    ShowVisited.stub(id: 1, pathImage: ""),
+    ShowVisited.stub(id: 2, pathImage: ""),
+    ShowVisited.stub(id: 3, pathImage: "")
+  ]
+}
+
+func buildGenres() -> [Genre] {
+  return [
+    Genre.stub(id: 1, name: "Genre 1"),
+    Genre.stub(id: 2, name: "Genre 2")
+  ]
+}

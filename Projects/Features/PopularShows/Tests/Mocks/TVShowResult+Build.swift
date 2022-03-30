5eab17cd18d6ea1a7decc99942bac13ef07f4344
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-29 21:52:50 -0500

    Style build models

--- Projects/Features/PopularShows/Tests/Mocks/TVShowResult+Build.swift
+++ Projects/Features/PopularShows/Tests/Mocks/TVShowResult+Build.swift
@@ -8,10 +8,18 @@
 import Shared
 
 func buildFirstPage() -> TVShowResult {
-  let firstShow = TVShow.stub(id: 1, name: "title1 🐶", posterPath: "/1",
-                              backDropPath: "/back1", overview: "overview")
-  let secondShow = TVShow.stub(id: 2, name: "title2 🔫", posterPath: "/2",
-                               backDropPath: "/back2", overview: "overview2")
+  let firstShow = TVShow.stub(id: 1,
+                              name: "Dark 🐶",
+                              voteAverage: 8.0,
+                              posterPath: "/1",
+                              backDropPath: "/back1",
+                              overview: "overview")
+  let secondShow = TVShow.stub(id: 2,
+                               name: "Dragon Ball Z 🔫",
+                               voteAverage: 9.0,
+                               posterPath: "/2",
+                               backDropPath: "/back2",
+                               overview: "overview2")
 
   return TVShowResult.stub(page: 1,
                            results: [firstShow, secondShow],
@@ -20,8 +28,12 @@ func buildFirstPage() -> TVShowResult {
 }
 
 func buildSecondPage() -> TVShowResult {
-  let thirdShow = TVShow.stub(id: 3, name: "title3 🚨", posterPath: "/3",
-                              backDropPath: "/back3", overview: "overview3")
+  let thirdShow = TVShow.stub(id: 3,
+                              name: "Esto es un TVShow con un título muy grande creado con fines de test 🚨",
+                              voteAverage: 10.0,
+                              posterPath: "/3",
+                              backDropPath: "/back3",
+                              overview: "overview3")
 
   return TVShowResult.stub(page: 2,
                            results: [thirdShow],

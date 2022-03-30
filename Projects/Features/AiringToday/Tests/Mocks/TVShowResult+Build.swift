2855544086dbdd2e5c0e7bfd7c04628904ee7e35
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-29 21:57:31 -0500

    Change access control

--- Projects/Features/AiringToday/Tests/Mocks/TVShowResult+Build.swift
+++ Projects/Features/AiringToday/Tests/Mocks/TVShowResult+Build.swift
@@ -8,18 +8,28 @@
 import Foundation
 @testable import Shared
 
-public func makeFirstPage() -> TVShowResult {
-  let firstShow = TVShow.stub(id: 1, name: "title1 🐶", posterPath: "/1",
-                              backDropPath: "/back1", overview: "overview")
+func makeFirstPage() -> TVShowResult {
+  let firstShow = TVShow.stub(
+    id: 1,
+    name: "title1 🐶",
+    posterPath: "/1",
+    backDropPath: "/back1",
+    overview: "overview"
+  )
   return TVShowResult.stub(page: 1,
                            results: [firstShow],
                            totalResults: 3,
                            totalPages: 2)
 }
 
-public func makeSecondPage() -> TVShowResult {
-  let thirdShow = TVShow.stub(id: 3, name: "title3 🚨", posterPath: "/3",
-                              backDropPath: "/back3", overview: "overview3")
+func makeSecondPage() -> TVShowResult {
+  let thirdShow = TVShow.stub(
+    id: 3,
+    name: "title3 🚨",
+    posterPath: "/3",
+    backDropPath: "/back3",
+    overview: "overview3"
+  )
   return TVShowResult.stub(page: 2,
                            results: [thirdShow],
                            totalResults: 3,

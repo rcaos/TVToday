38069ca0497f60c577bea8a07ed4fad95e4e2f6d
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-31 17:42:12 -0500

    Add reminder to test cases

--- Projects/Features/ShowDetails/Tests/SeasonsScene/Presentation/ViewModel/EpisodesListViewModelTests.swift
+++ Projects/Features/ShowDetails/Tests/SeasonsScene/Presentation/ViewModel/EpisodesListViewModelTests.swift
@@ -289,7 +289,7 @@ class EpisodesListViewModelTests: XCTestCase {
     //      }
   }
 
-  func test_When_Ask_For_Different_Season_And_UseCase_Return_OK_ViewModel_Should_Contains_Episodes() {
+  func test_When_Ask_For_Different_Season_And_UseCase_Return_OK_ViewModel_Should_Contains_populated() {
     // given
 //    let statesObserver = scheduler.createObserver(EpisodesListViewModel.ViewState.self)
 
@@ -332,77 +332,76 @@ class EpisodesListViewModelTests: XCTestCase {
     //
     //          expect(statesObserver.events).toEventually(equal(expected))
   }
-}
 
-//      context("When Ask for Diferent Season And UseCase returns Episodes") {
-//        it("Should ViewModel contains List of Episodes") {
-//
-//          // given
-//          let episodesObserver = scheduler.createObserver([SeasonsSectionModel].self)
-//
-//          let numberOfSeasons = 9
-//
-//          let firstEpisodes = self.episodes.map { EpisodeSectionModelType(episode: $0)}
-//            .map { SeasonsSectionItem.episodes(items: $0) }
-//          let headerViewModel = SeasonHeaderViewModel(showDetail: self.detailResult)
-//
-//          let firstSeason: [SeasonsSectionModel] = [
-//            .headerShow(header: "Header", items: [.headerShow(viewModel: headerViewModel)]),
-//            .seasons(header: "Seasons", items: [.seasons(number: numberOfSeasons)]),
-//            .episodes(header: "Episodes", items: firstEpisodes)
-//          ]
-//
-//          let loadingSection: [SeasonsSectionModel] = [
-//            .headerShow(header: "Header", items: [.headerShow(viewModel: headerViewModel)]),
-//            .seasons(header: "Seasons", items: [.seasons(number: numberOfSeasons)]),
-//            .episodes(header: "Episodes", items: [])
-//          ]
-//
-//          let secondEpisodes = self.episodes.map { EpisodeSectionModelType(episode: $0)}
-//            .map { SeasonsSectionItem.episodes(items: $0) }
-//          let secondSeason: [SeasonsSectionModel] = [
-//            .headerShow(header: "Header", items: [.headerShow(viewModel: headerViewModel)]),
-//            .seasons(header: "Seasons", items: [.seasons(number: numberOfSeasons)]),
-//            .episodes(header: "Episodes", items: secondEpisodes)
-//          ]
-//
-//          let seasonResult = SeasonResult(id: "1", episodes: self.episodes, seasonNumber: 1)
-//
-//          fetchTVShowDetailsUseCaseMock.result = self.detailResult
-//          fetchEpisodesUseCaseMock.result = seasonResult
-//
-//          let viewModel: EpisodesListViewModelProtocol =
-//            EpisodesListViewModel(tvShowId: 1,
-//                                  fetchDetailShowUseCase: fetchTVShowDetailsUseCaseMock,
-//                                  fetchEpisodesUseCase: fetchEpisodesUseCaseMock)
-//
-//          viewModel.data
-//            .distinctUntilChanged()
-//            .subscribe { event in
-//              episodesObserver.on(event)
-//            }
-//            .disposed(by: disposeBag)
-//
-//          let seasonViewModel = SeasonListViewModelMock()
-//
-//          // when
-//          viewModel.viewDidLoad()
-//
-//          let secondSeasonResult = SeasonResult(id: "2", episodes: self.episodes, seasonNumber: 2)
-//          fetchEpisodesUseCaseMock.result = secondSeasonResult
-//          fetchEpisodesUseCaseMock.error = nil
-//
-//          // select next Season
-//          viewModel.seasonListViewModel(seasonViewModel, didSelectSeason: 2)
-//
-//          // when
-//          let expected: [Recorded<Event<[SeasonsSectionModel]>>] = [
-//            .next(0, []),
-//            .next(0, firstSeason),
-//            .next(0, loadingSection),
-//            .next(0, secondSeason)
-//          ]
-//
-//          expect(episodesObserver.events).toEventually(equal(expected))
-//        }
-//      }
+  func test_When_Ask_For_Different_Season_And_UseCase_Return_OK_ViewModel_Should_Contains_Data_With_Episodes() {
+    // given
+//    let episodesObserver = scheduler.createObserver([SeasonsSectionModel].self)
+
+    let numberOfSeasons = 9
+
+    let firstEpisodes = self.episodes
+      .map { EpisodeSectionModelType(episode: $0)}
+      .map { SeasonsSectionItem.episodes(items: $0) }
+    let headerViewModel = SeasonHeaderViewModel(showDetail: self.detailResult)
+
+    let firstSeason: [SeasonsSectionModel] = [
+      .headerShow(items: [.headerShow(viewModel: headerViewModel)]),
+      .seasons(items: [.seasons]),
+      .episodes(items: firstEpisodes)
+    ]
+
+    let loadingSection: [SeasonsSectionModel] = [
+      .headerShow(items: [.headerShow(viewModel: headerViewModel)]),
+      .seasons(items: [.seasons]),
+      .episodes(items: [])
+    ]
+
+    let secondEpisodes = self.episodes
+      .map { EpisodeSectionModelType(episode: $0)}
+      .map { SeasonsSectionItem.episodes(items: $0) }
+    let secondSeason: [SeasonsSectionModel] = [
+      .headerShow(items: [.headerShow(viewModel: headerViewModel)]),
+      .seasons(items: [.seasons]),
+      .episodes(items: secondEpisodes)
+    ]
+
+    let seasonResult = SeasonResult(id: "1", episodes: self.episodes, seasonNumber: 1)
+
+    fetchTVShowDetailsUseCaseMock.result = self.detailResult
+    fetchEpisodesUseCaseMock.result = seasonResult
+
+    let sut: EpisodesListViewModelProtocol =
+    EpisodesListViewModel(tvShowId: 1,
+                          fetchDetailShowUseCase: fetchTVShowDetailsUseCaseMock,
+                          fetchEpisodesUseCase: fetchEpisodesUseCaseMock)
+    //
+    //          viewModel.data
+    //            .distinctUntilChanged()
+    //            .subscribe { event in
+    //              episodesObserver.on(event)
+    //            }
+    //            .disposed(by: disposeBag)
+    //
+    //          let seasonViewModel = SeasonListViewModelMock()
+    //
+    //          // when
+    //          viewModel.viewDidLoad()
+    //
+    //          let secondSeasonResult = SeasonResult(id: "2", episodes: self.episodes, seasonNumber: 2)
+    //          fetchEpisodesUseCaseMock.result = secondSeasonResult
+    //          fetchEpisodesUseCaseMock.error = nil
+    //
+    //          // select next Season
+    //          viewModel.seasonListViewModel(seasonViewModel, didSelectSeason: 2)
+    //
+    //          // when
+    //          let expected: [Recorded<Event<[SeasonsSectionModel]>>] = [
+    //            .next(0, []),
+    //            .next(0, firstSeason),
+    //            .next(0, loadingSection),
+    //            .next(0, secondSeason)
+    //          ]
+    //
+    //          expect(episodesObserver.events).toEventually(equal(expected))
+  }
+}

718438cbd9e8f0d54f7377b16c4e3c5f9cb36e17
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-03-30 22:05:52 -0500

    Fix mocks

--- Projects/Features/ShowDetails/Tests/DetailsScene/Mocks/ViewModel/TVShowDetailViewModel+Mock.swift
+++ Projects/Features/ShowDetails/Tests/DetailsScene/Mocks/ViewModel/TVShowDetailViewModel+Mock.swift
@@ -5,32 +5,27 @@
 //  Created by Jeans Ruiz on 8/6/20.
 //
 
-import RxSwift
+import Combine
 @testable import ShowDetails
 
 class TVShowDetailViewModelMock: TVShowDetailViewModelProtocol {
-
   // MARK: - Input
   func viewDidLoad() { }
   func refreshView() { }
   func viewDidFinish() { }
 
-  var tapFavoriteButton = PublishSubject<Void>()
-  var tapWatchedButton = PublishSubject<Void>()
+  func favoriteButtonDidTapped() { }
+  func watchedButtonDidTapped() { }
 
   // MARK: - Output
-
   func isUserLogged() -> Bool { return true }
   func navigateToSeasons() { }
 
-  var viewState: Observable<TVShowDetailViewModel.ViewState>
-  var isFavorite = Observable<Bool>.just(false)
-  var isWatchList = Observable<Bool>.just(false)
-
-  private var viewStateObservableSubject: BehaviorSubject<TVShowDetailViewModel.ViewState>
+  var viewState: CurrentValueSubject<TVShowDetailViewModel.ViewState, Never>
+  var isFavorite = CurrentValueSubject<Bool, Never>(false)
+  var isWatchList = CurrentValueSubject<Bool, Never>(false)
 
   init(state: TVShowDetailViewModel.ViewState) {
-    viewStateObservableSubject = BehaviorSubject(value: state)
-    viewState = viewStateObservableSubject.asObservable()
+    viewState = CurrentValueSubject(state)
   }
 }

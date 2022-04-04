444f7bc79a19a7534043f7c46361d773b5b00764
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-03 20:10:01 -0500

    Test tap on favorite button

--- Projects/Features/ShowDetails/Tests/DetailsScene/Presentation/ViewModel/TVShowDetailViewModelTapsTests.swift
+++ Projects/Features/ShowDetails/Tests/DetailsScene/Presentation/ViewModel/TVShowDetailViewModelTapsTests.swift
@@ -6,6 +6,7 @@
 //
 
 import Combine
+import CombineSchedulers
 import XCTest
 
 @testable import ShowDetails
@@ -35,55 +36,42 @@ class TVShowDetailViewModelTapsTests: XCTestCase {
     disposeBag = []
   }
 
-  // MARK: - TODO, test this tap cases 👇
-
   func test_When_ViewModel_Receive_isFavorite_Tap_Observable_Should_Change() {
     // given
-//    let initialFavoriteState = true
-//
-//    fetchTVShowDetailsUseCaseMock.result = self.detailResult
-//    fetchTVAccountStateMock.result = TVShowAccountStateResult.stub(id: 1, isFavorite: initialFavoriteState, isWatchList: false)
-//
-//    let scheduler = TestScheduler(initialClock: 0)
-//    let favoriteObserver = scheduler.createObserver(Bool.self)
-//
-//    markAsFavoriteUseCaseMock.result = !initialFavoriteState
-//
-//    let viewModel: TVShowDetailViewModelProtocol = TVShowDetailViewModel(
-//      1,
-//      fetchLoggedUser: fetchLoggedUserMock,
-//      fetchDetailShowUseCase: fetchTVShowDetailsUseCaseMock,
-//      fetchTvShowState: fetchTVAccountStateMock,
-//      markAsFavoriteUseCase: markAsFavoriteUseCaseMock,
-//      saveToWatchListUseCase: saveToWatchListUseCaseMock,
-//      coordinator: nil
-//    )
-//
-//    viewModel.isFavorite
-//      .subscribe { event in
-//        favoriteObserver.on(event)
-//      }
-//      .disposed(by: disposeBag)
-//
-//    // when
-//    viewModel.viewDidLoad()
-//
-//    // Emit taps from View
-//    scheduler.createColdObservable([.next(10, ())])
-//      .subscribe { event in
-//        viewModel.tapFavoriteButton.on(event)
-//      }
-//      .disposed(by: disposeBag)
-//    scheduler.start()
-//
-//    // then
-//    let expected: [Recorded<Event<Bool>>] =
-//    [.next(0, false),
-//     .next(0, initialFavoriteState),
-//     .next(10, !initialFavoriteState)]
-//
-//    expect(favoriteObserver.events)
-//      .toEventually(equal(expected), timeout: .milliseconds(500))
+    let initialFavoriteState = true
+
+    fetchTVShowDetailsUseCaseMock.result = self.detailResult
+    fetchTVAccountStateMock.result = TVShowAccountStateResult.stub(id: 1, isFavorite: initialFavoriteState, isWatchList: false)
+
+    let scheduler = DispatchQueue.test
+    markAsFavoriteUseCaseMock.result = !initialFavoriteState
+
+    let sut: TVShowDetailViewModelProtocol = TVShowDetailViewModel(
+      1,
+      fetchLoggedUser: fetchLoggedUserMock,
+      fetchDetailShowUseCase: fetchTVShowDetailsUseCaseMock,
+      fetchTvShowState: fetchTVAccountStateMock,
+      markAsFavoriteUseCase: markAsFavoriteUseCaseMock,
+      saveToWatchListUseCase: saveToWatchListUseCaseMock,
+      coordinator: nil,
+      scheduler: scheduler.eraseToAnyScheduler()
+    )
+
+    let expected = [false, initialFavoriteState, !initialFavoriteState]
+    var received = [Bool]()
+
+    sut.isFavorite.removeDuplicates()
+      .sink(receiveValue: { received.append($0) }).store(in: &disposeBag)
+
+    // when
+    sut.viewDidLoad()
+    scheduler.advance(by: 1)
+
+    sut.favoriteButtonDidTapped()
+    scheduler.advance(by: .milliseconds(300))
+
+    // then
+    XCTAssertEqual(expected, received, "Should contains Populated State")
   }
 
   func test_When_ViewModel_Receive_isWatched_Tap_Observable_Should_Change() {

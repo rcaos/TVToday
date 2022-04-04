9886e8b072ce9a0f986105bb58da2da32bcf687f
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-03 20:14:54 -0500

    Test initial favorite to False

--- Projects/Features/ShowDetails/Tests/DetailsScene/Presentation/ViewModel/TVShowDetailViewModelTapsTests.swift
+++ Projects/Features/ShowDetails/Tests/DetailsScene/Presentation/ViewModel/TVShowDetailViewModelTapsTests.swift
@@ -12,7 +12,7 @@ import XCTest
 @testable import ShowDetails
 @testable import Shared
 
-class TVShowDetailViewModelTapsTests: XCTestCase {
+class TVShowDetailViewModelFavoriteTapsTests: XCTestCase {
 
   let detailResult = TVShowDetailResult.stub()
 
@@ -57,7 +57,7 @@ class TVShowDetailViewModelTapsTests: XCTestCase {
       scheduler: scheduler.eraseToAnyScheduler()
     )
 
-    let expected = [false, initialFavoriteState, !initialFavoriteState]
+    let expected = [false, true, false]
     var received = [Bool]()
 
     sut.isFavorite.removeDuplicates()
@@ -71,56 +71,44 @@ class TVShowDetailViewModelTapsTests: XCTestCase {
     scheduler.advance(by: .milliseconds(300))
 
     // then
-    XCTAssertEqual(expected, received, "Should contains Populated State")
+    XCTAssertEqual(expected, received, "Should contains Favorite State")
   }
 
-  func test_When_ViewModel_Receive_isWatched_Tap_Observable_Should_Change() {
-//    // given
-//    let initialStateWatched = false
-//
-//    fetchTVShowDetailsUseCaseMock.result = self.detailResult
-//    fetchTVAccountStateMock.result = TVShowAccountStateResult.stub(id: 1, isFavorite: true, isWatchList: initialStateWatched)
-//
-//    let scheduler = TestScheduler(initialClock: 0)
-//    let disposeBag = DisposeBag()
-//    let watchedObserver = scheduler.createObserver(Bool.self)
-//
-//    saveToWatchListUseCaseMock.result = !initialStateWatched
-//
-//    let viewModel: TVShowDetailViewModelProtocol = TVShowDetailViewModel(
-//      1,
-//      fetchLoggedUser: fetchLoggedUserMock,
-//      fetchDetailShowUseCase: fetchTVShowDetailsUseCaseMock,
-//      fetchTvShowState: fetchTVAccountStateMock,
-//      markAsFavoriteUseCase: markAsFavoriteUseCaseMock,
-//      saveToWatchListUseCase: saveToWatchListUseCaseMock,
-//      coordinator: nil
-//    )
-//
-//    viewModel.isWatchList
-//      .subscribe { event in
-//        watchedObserver.on(event)
-//      }
-//      .disposed(by: disposeBag)
-//
-//    // when
-//    viewModel.viewDidLoad()
-//
-//    // Emit taps from View
-//    scheduler.createColdObservable([.next(10, ())])
-//      .subscribe { event in
-//        viewModel.tapWatchedButton.on(event)
-//      }
-//      .disposed(by: disposeBag)
-//    scheduler.start()
-//
-//    // then
-//    let expected: [Recorded<Event<Bool>>] =
-//    [.next(0, false),
-//     .next(0, initialStateWatched),
-//     .next(10, !initialStateWatched)]
-//
-//    expect(watchedObserver.events)
-//      .toEventually(equal(expected), timeout: .milliseconds(500))
+  func test_When_ViewModel_Receive_isFavorite_Tap_Observable_Should_Change_to_True() {
+    // given
+    let initialFavoriteState = false
+
+    fetchTVShowDetailsUseCaseMock.result = self.detailResult
+    fetchTVAccountStateMock.result = TVShowAccountStateResult.stub(id: 1, isFavorite: initialFavoriteState, isWatchList: false)
+
+    let scheduler = DispatchQueue.test
+    markAsFavoriteUseCaseMock.result = !initialFavoriteState
+
+    let sut: TVShowDetailViewModelProtocol = TVShowDetailViewModel(
+      1,
+      fetchLoggedUser: fetchLoggedUserMock,
+      fetchDetailShowUseCase: fetchTVShowDetailsUseCaseMock,
+      fetchTvShowState: fetchTVAccountStateMock,
+      markAsFavoriteUseCase: markAsFavoriteUseCaseMock,
+      saveToWatchListUseCase: saveToWatchListUseCaseMock,
+      coordinator: nil,
+      scheduler: scheduler.eraseToAnyScheduler()
+    )
+
+    let expected = [false, true]
+    var received = [Bool]()
+
+    sut.isFavorite.removeDuplicates()
+      .sink(receiveValue: { received.append($0) }).store(in: &disposeBag)
+
+    // when
+    sut.viewDidLoad()
+    scheduler.advance(by: 1)
+
+    sut.favoriteButtonDidTapped()
+    scheduler.advance(by: .milliseconds(300))
+
+    // then
+    XCTAssertEqual(expected, received, "Should contains Favorite State true")
   }
 }

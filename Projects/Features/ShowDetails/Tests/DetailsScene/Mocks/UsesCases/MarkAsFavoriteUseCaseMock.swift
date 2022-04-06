c6cc44e2780a9a5fa3fad0ad3d8e5f048ae8b5a8
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-05 20:22:27 -0500

    Refactor usecase tap mock

--- Projects/Features/ShowDetails/Tests/DetailsScene/Mocks/UsesCases/MarkAsFavoriteUseCaseMock.swift
+++ Projects/Features/ShowDetails/Tests/DetailsScene/Mocks/UsesCases/MarkAsFavoriteUseCaseMock.swift
@@ -11,18 +11,22 @@ import ShowDetails
 import Shared
 
 class MarkAsFavoriteUseCaseMock: MarkAsFavoriteUseCase {
+  let subject = PassthroughSubject<Bool, DataTransferError>()
   var result: Bool?
   var error: DataTransferError?
-
+  var calledCounter = 0
+  
   func execute(requestValue: MarkAsFavoriteUseCaseRequestValue) -> AnyPublisher<Bool, DataTransferError> {
+    calledCounter += 1
+    
     if let error = error {
       return Fail(error: error).eraseToAnyPublisher()
     }
-
+    
     if let result = result {
       return Just(result).setFailureType(to: DataTransferError.self).eraseToAnyPublisher()
     }
-
-    return Empty().setFailureType(to: DataTransferError.self).eraseToAnyPublisher()
+    
+    return subject.eraseToAnyPublisher()
   }
 }

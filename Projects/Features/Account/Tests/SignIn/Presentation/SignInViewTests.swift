0dcaafcd80e315fdfdda23dbf9adcdca1d650173
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-08 10:57:25 -0500

    Unifigy test suite

--- Projects/Features/Account/Tests/SignIn/Presentation/SignInViewTests.swift
+++ Projects/Features/Account/Tests/SignIn/Presentation/SignInViewTests.swift
@@ -1,5 +1,5 @@
 //
-//  SignInViewDarkTests.swift
+//  SignInViewTests.swift
 //  AccountTests
 //
 //  Created by Jeans Ruiz on 20/12/21.
@@ -9,7 +9,7 @@ import XCTest
 import SnapshotTesting
 @testable import Account
 
-class SignInViewDarkTests: XCTestCase {
+class SignInViewTests: XCTestCase {
 
   override func setUp() {
     super.setUp()
@@ -19,33 +19,55 @@ class SignInViewDarkTests: XCTestCase {
   func test_WhenViewIsInitial_thenShowInitialScreen() {
     // given
     let signInViewModel = SignInViewModelMock(state: .initial)
+
+    // when
     let viewController = SignInViewController(viewModel: signInViewModel)
-    viewController.overrideUserInterfaceStyle = .dark
-    _ = viewController.view
+    configureWith(viewController, style: .dark)
 
     // then
     assertSnapshot(matching: viewController, as: .wait(for: 0.01, on: .image(on: .iPhoneSe)))
+
+    // when
+    let lightViewController = SignInViewController(viewModel: signInViewModel)
+    configureWith(lightViewController, style: .light)
+
+    // then
+    assertSnapshot(matching: lightViewController, as: .wait(for: 0.01, on: .image(on: .iPhoneSe)))
   }
 
   func test_WhenViewIsLoading_thenShowLoadingScreen() {
     // given
     let signInViewModel = SignInViewModelMock(state: .loading)
+
+    // when
     let viewController = SignInViewController(viewModel: signInViewModel)
-    viewController.overrideUserInterfaceStyle = .dark
-    _ = viewController.view
+    configureWith(viewController, style: .dark)
 
     // then
     assertSnapshot(matching: viewController, as: .wait(for: 0.01, on: .image(on: .iPhoneSe)))
+
+    // when
+    let lightViewController = SignInViewController(viewModel: signInViewModel)
+    configureWith(lightViewController, style: .light)
+
+    // then
+    assertSnapshot(matching: lightViewController, as: .wait(for: 0.01, on: .image(on: .iPhoneSe)))
   }
 
   func test_WhenViewIsError_thenShowErrorScreen() {
     // given
     let signInViewModel = SignInViewModelMock(state: .error)
     let viewController = SignInViewController(viewModel: signInViewModel)
-    viewController.overrideUserInterfaceStyle = .dark
-    _ = viewController.view
+    configureWith(viewController, style: .dark)
 
     // then
     assertSnapshot(matching: viewController, as: .wait(for: 0.01, on: .image(on: .iPhoneSe)))
+
+    // when
+    let lightViewController = SignInViewController(viewModel: signInViewModel)
+    configureWith(lightViewController, style: .light)
+
+    // then
+    assertSnapshot(matching: lightViewController, as: .wait(for: 0.01, on: .image(on: .iPhoneSe)))
   }
 }

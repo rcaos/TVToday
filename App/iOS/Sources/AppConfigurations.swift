7fea79854ad9fda5b0eb0ecfcd033e440b1e7b88
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-19 13:04:38 -0500

    Move app files to app folder

---
+++ App/iOS/Sources/AppConfigurations.swift
@@ -0,0 +1,38 @@
+//
+//  AppConfigurations.swift
+//  TVToday
+//
+//  Created by Jeans Ruiz on 1/14/20.
+//  Copyright © 2020 Jeans. All rights reserved.
+//
+
+import Foundation
+
+final class AppConfigurations {
+
+  lazy var apiKey: String = {
+    guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
+      fatalError("ApiKey must not be empty in plist")
+    }
+    return apiKey
+  }()
+
+  lazy var apiBaseURL: URL = {
+    guard let apiBaseString = Bundle.main.object(forInfoDictionaryKey: "API_BASE_URL") as? String else {
+      fatalError("ApiBaseURL must not be empty in plist")
+    }
+
+    guard let apiBaseURL = URL(string: apiBaseString) else {
+      fatalError("Could not convert \(apiBaseString) into a URL")
+    }
+
+    return apiBaseURL
+  }()
+
+  lazy var imagesBaseURL: String = {
+    guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "IMAGE_BASE_URL") as? String else {
+      fatalError("ApiBaseURL must not be empty in plist")
+    }
+    return imageBaseURL
+  }()
+}

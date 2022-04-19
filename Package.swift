1a1ff448c2daa3611c5651fd39825e88bd324da1
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-19 13:51:37 -0500

    Update reference and create AppFeature

--- Package.swift
+++ Package.swift
@@ -4,24 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "TVToday",
-    products: [
-        .library(
-            name: "TVToday",
-            targets: ["TVToday"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TVToday",
-            dependencies: []),
-        .testTarget(
-            name: "TVTodayTests",
-            dependencies: ["TVToday"]),
-    ]
+  name: "TVToday",
+  products: [
+    .library(
+      name: "AppFeature", targets: ["AppFeature"]
+    )
+  ],
+  dependencies: [
+
+  ],
+  targets: [
+    .target(
+      name: "AppFeature",
+      dependencies: [
+
+      ]),
+    .testTarget(name: "AppFeatureTests", dependencies: ["AppFeature"])
+  ]
 )

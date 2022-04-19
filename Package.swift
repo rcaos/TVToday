31762af1c4c5e1b65200180ec24f39ab7f6c3a75
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-19 14:31:35 -0500

    Add keychain dependency

--- Package.swift
+++ Package.swift
@@ -10,26 +10,34 @@ let package = Package(
   ],
   products: [
     .library(name: "AppFeature", targets: ["AppFeature"]),
-    .library(name: "UI", targets: ["UI"]),
+    .library(name: "KeyChainStorage", targets: ["KeyChainStorage"]),
     .library(name: "Networking", targets: ["Networking"]),
-    .library(name: "NetworkingInterface", targets: ["NetworkingInterface"])
+    .library(name: "NetworkingInterface", targets: ["NetworkingInterface"]),
+    .library(name: "UI", targets: ["UI"])
   ],
   dependencies: [
-
+    .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "14.0.0")
   ],
   targets: [
     .target(
       name: "AppFeature",
       dependencies: [
         // TODO, check graph here
-        "UI",
+        "KeyChainStorage",
         "Networking",
-        "NetworkingInterface"
+        "NetworkingInterface",
+        "UI",
       ]),
     .testTarget(name: "AppFeatureTests", dependencies: ["AppFeature"]),
-    .target(name: "UI", resources: [.process("Resources/")]),
+    .target(
+      name: "KeyChainStorage",
+      dependencies: [
+        .product(name: "KeychainSwift", package: "keychain-swift")
+      ]
+    ),
     .target(name: "Networking", dependencies: ["NetworkingInterface"]),
     .testTarget(name: "NetworkingTests", dependencies: ["Networking"]),
-    .target(name: "NetworkingInterface")
+    .target(name: "NetworkingInterface"),
+    .target(name: "UI", resources: [.process("Resources/")]),
   ]
 )

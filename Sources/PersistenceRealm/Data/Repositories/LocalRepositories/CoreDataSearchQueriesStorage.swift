8f61995804add2549a7151a86605c14fb45abe4a
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-25 17:22:33 -0500

    Return recent searchs

--- Sources/PersistenceRealm/Data/Repositories/LocalRepositories/CoreDataSearchQueriesStorage.swift
+++ Sources/PersistenceRealm/Data/Repositories/LocalRepositories/CoreDataSearchQueriesStorage.swift
@@ -17,6 +17,15 @@ public final class CoreDataSearchQueriesStorage {
   public init(coreDataStorage: CoreDataStorage) {
     self.coreDataStorage = coreDataStorage
   }
+
+  // MARK: - Private
+  private func find(for userId: Int) -> NSFetchRequest<CDRecentSearch> {
+    let request: NSFetchRequest = CDRecentSearch.fetchRequest()
+    // MARK: - TODO, .sorted(byKeyPath: "date", ascending: false).map { $0 }
+    request.predicate = NSPredicate(format: "%K = %d",
+                                    #keyPath(CDRecentSearch.userId), userId)
+    return request
+  }
 }
 
 extension CoreDataSearchQueriesStorage: SearchLocalRepository {
@@ -39,16 +48,21 @@ extension CoreDataSearchQueriesStorage: SearchLocalRepository {
   }
 
   public func fetchSearchs(userId: Int) -> AnyPublisher<[Search], CustomError> {
-    return Fail(error: CustomError.genericError).eraseToAnyPublisher()
-
-    // MARK: - TODO,
-    // coreDataStorage.performBackgroundTask { context in }
-
-
-//    return Just(
-//      store.find(for: userId).map { $0.asDomain() }
-//    )
-//      .setFailureType(to: CustomError.self)
-//      .eraseToAnyPublisher()
+    return Deferred { [coreDataStorage] in
+      return Future<[Search], CustomError> { promise in
+        coreDataStorage.performBackgroundTask { context in
+          do {
+            let fetchRequest = self.find(for: userId)
+            let results = try context.fetch(fetchRequest)
+            let domain = results.map { $0.toDomain() }
+            promise(.success(domain))
+          } catch {
+            debugPrint("CoreDataSearchQueriesStorage Unresolved error \(error), \((error as NSError).userInfo)")
+            promise(.failure(.genericError))
+          }
+        }
+      }
+    }
+    .eraseToAnyPublisher()
   }
 }

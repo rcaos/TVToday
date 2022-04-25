e4c96354749bbce52a1ef8f894a5419bdc57feb8
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-25 17:58:47 -0500

    Add helpers to domain entity

--- Sources/PersistenceRealm/Domain/Entities/CDShowVisited.swift
+++ Sources/PersistenceRealm/Domain/Entities/CDShowVisited.swift
@@ -6,20 +6,31 @@
 //
 
 import CoreData
+import Persistence
 
 final class CDShowVisited: NSManagedObject {
 
-  @NSManaged private(set) var id: String
+  @NSManaged private(set) var id: Int
   @NSManaged private(set) var createdAt: Date
   @NSManaged private(set) var pathImage: String
   @NSManaged private(set) var userId: Int
 
-  static func insert(into context: NSManagedObjectContext, pathImage: String, userId: Int) -> CDShowVisited {
+  static func insert(into context: NSManagedObjectContext, showId: Int, pathImage: String, userId: Int) -> CDShowVisited {
     let showVisited: CDShowVisited = context.insertObject()
-    showVisited.id = UUID().uuidString
+    showVisited.id = showId
     showVisited.createdAt = Date()
     showVisited.pathImage = pathImage
     showVisited.userId = userId
     return showVisited
+  }
+
+  @nonobjc public class func fetchRequest() -> NSFetchRequest<CDShowVisited> {
+    return NSFetchRequest<CDShowVisited>(entityName: "CDShowVisited")
+  }
+}
+
+extension CDShowVisited {
+  func toDomain() -> ShowVisited {
+    return ShowVisited(id: id, pathImage: pathImage)
   }
 }

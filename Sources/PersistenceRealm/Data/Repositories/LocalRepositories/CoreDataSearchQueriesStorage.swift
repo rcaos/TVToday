84a74aba652709b71c0c7a519905a037f091eefe
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-25 17:53:13 -0500

    Remove extra method

--- Sources/PersistenceRealm/Data/Repositories/LocalRepositories/CoreDataSearchQueriesStorage.swift
+++ Sources/PersistenceRealm/Data/Repositories/LocalRepositories/CoreDataSearchQueriesStorage.swift
@@ -17,15 +17,6 @@ public final class CoreDataSearchQueriesStorage {
   public init(coreDataStorage: CoreDataStorage) {
     self.coreDataStorage = coreDataStorage
   }
-
-  // MARK: - Private
-  private func find(for userId: Int) -> NSFetchRequest<CDRecentSearch> {
-    let request: NSFetchRequest = CDRecentSearch.fetchRequest()
-    // MARK: - TODO, .sorted(byKeyPath: "date", ascending: false).map { $0 }
-    request.predicate = NSPredicate(format: "%K = %d",
-                                    #keyPath(CDRecentSearch.userId), userId)
-    return request
-  }
 }
 
 extension CoreDataSearchQueriesStorage: SearchLocalRepository {
@@ -53,10 +44,12 @@ extension CoreDataSearchQueriesStorage: SearchLocalRepository {
       return Future<[Search], CustomError> { promise in
         coreDataStorage.performBackgroundTask { context in
           do {
-            let fetchRequest = self.find(for: userId)
-            let results = try context.fetch(fetchRequest)
-            let domain = results.map { $0.toDomain() }
-            promise(.success(domain))
+            let request: NSFetchRequest = CDRecentSearch.fetchRequest()
+            // MARK: - TODO, .sorted(byKeyPath: "date", ascending: false).map { $0 }
+            request.predicate = NSPredicate(format: "%K = %d", #keyPath(CDRecentSearch.userId), userId)
+
+            let results = try context.fetch(request).map { $0.toDomain() }
+            promise(.success(results))
           } catch {
             debugPrint("CoreDataSearchQueriesStorage Unresolved error \(error), \((error as NSError).userInfo)")
             promise(.failure(.genericError))

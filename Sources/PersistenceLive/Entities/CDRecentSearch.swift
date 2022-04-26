6a6a3950d88c28b8161a091f30d4ae796f3ffe12
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-26 18:34:18 -0500

    Create Store for entity

--- Sources/PersistenceLive/Entities/CDRecentSearch.swift
+++ Sources/PersistenceLive/Entities/CDRecentSearch.swift
@@ -9,12 +9,12 @@ import CoreData
 import Persistence
 
 final class CDRecentSearch: NSManagedObject {
-
+  
   @NSManaged private(set) var id: String
   @NSManaged private(set) var query: String
   @NSManaged private(set) var createdAt: Date
   @NSManaged private(set) var userId: Int
-
+  
   static func insert(into context: NSManagedObjectContext, query: String, userId: Int) -> CDRecentSearch {
     let recentSearch: CDRecentSearch = context.insertObject()
     recentSearch.id = UUID().uuidString
@@ -23,34 +23,16 @@ final class CDRecentSearch: NSManagedObject {
     recentSearch.createdAt = Date()
     return recentSearch
   }
-
-  @nonobjc public class func fetchRequest() -> NSFetchRequest<CDRecentSearch> {
-    return NSFetchRequest<CDRecentSearch>(entityName: "CDRecentSearch")
-  }
-}
-
-// MARK: -  Move to Helper
-extension NSManagedObjectContext {
-
-//  func insertObject<A: NSManagedObject>() -> A where A: Managed {
-  func insertObject<A: NSManagedObject>() -> A {
-    guard let obj = NSEntityDescription.insertNewObject(forEntityName: A.entityName, into: self) as? A else {
-      fatalError("Wrong object type")
-    }
-    return obj
-  }
-}
-
-
-extension NSManagedObject {
-  static var entityName: String {
-    return entity().name!
-  }
 }
 
 extension CDRecentSearch {
-
   func toDomain() -> Search {
     return Search(query: query)
+  }
+}
+
+extension CDRecentSearch: Managed {
+  static var defaultSortDescriptors: [NSSortDescriptor] {
+    return [NSSortDescriptor(key: #keyPath(createdAt), ascending: false)]
   }
 }

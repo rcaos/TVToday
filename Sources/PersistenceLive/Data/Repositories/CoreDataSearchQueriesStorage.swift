806280f5a312c3dc6e5bda1130e07e49f36d03d7
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-04-26 12:45:04 -0500

    Return data in order

--- Sources/PersistenceLive/Data/Repositories/CoreDataSearchQueriesStorage.swift
+++ Sources/PersistenceLive/Data/Repositories/CoreDataSearchQueriesStorage.swift
@@ -52,8 +52,8 @@ extension CoreDataSearchQueriesStorage: SearchLocalRepository {
         coreDataStorage.performBackgroundTask { context in
           do {
             let request: NSFetchRequest = CDRecentSearch.fetchRequest()
-            // MARK: - TODO, .sorted(byKeyPath: "date", ascending: false).map { $0 }
             request.predicate = NSPredicate(format: "%K = %d", #keyPath(CDRecentSearch.userId), userId)
+            request.sortDescriptors = [NSSortDescriptor(key: #keyPath(CDRecentSearch.createdAt), ascending: false)]
 
             let results = try context.fetch(request).map { $0.toDomain() }
             promise(.success(results))

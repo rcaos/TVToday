f6839ca144ecdefeb6448a8cfc65610d63f7cfff
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-05-10 15:38:29 -0500

    Move token repository inside Auth repository

--- Sources/AccountFeature/Data/Repositories/DefaultAuthRepository.swift
+++ Sources/AccountFeature/Data/Repositories/DefaultAuthRepository.swift
@@ -7,26 +7,52 @@
 //
 
 import Combine
+import Foundation
 import NetworkingInterface
 import Networking
+import Shared
 
 final class DefaultAuthRepository {
   private let remoteDataSource: AuthRemoteDataSource
+  private let requestTokenRepository: RequestTokenRepository
+  private let accessTokenRepository: AccessTokenRepository
 
-  init(remoteDataSource: AuthRemoteDataSource) {
+  init(remoteDataSource: AuthRemoteDataSource,
+       requestTokenRepository: RequestTokenRepository,
+       accessTokenRepository: AccessTokenRepository) {
     self.remoteDataSource = remoteDataSource
+    self.requestTokenRepository = requestTokenRepository
+    self.accessTokenRepository = accessTokenRepository
   }
 }
 
 // MARK: - AuthRepository
 extension DefaultAuthRepository: AuthRepository {
 
+
   func requestToken() -> AnyPublisher<NewRequestToken, DataTransferError> {
     return remoteDataSource.requestToken()
-      .map {
-        return NewRequestToken(success: $0.success, token: $0.token)
+      .tryMap { result -> NewRequestToken in
+        // MARK: - TODO, Inject URL base instead
+        let newToken = try self.mapRequestToken(basePath: "https://www.themoviedb.org/authenticate", result: result)
+        self.requestTokenRepository.saveRequestToken (result.token)
+        return newToken
+      }
+      .mapError { error -> DataTransferError in
+        return DataTransferError.noResponse // MARk: - TODO, change error
       }
       .eraseToAnyPublisher()
+  }
+
+  // MARK: - TODO, move to Mapping file
+  private func mapRequestToken(basePath: String, result: NewRequestTokenDTO) throws -> NewRequestToken {
+    if result.success == true,
+       let url = URL(string: "\(basePath)\(result.token)") {
+      return NewRequestToken(token: result.token, url: url)
+    } else {
+      print("cannot Convert request token= \(result), basePath=\(basePath)")
+      throw DataTransferError.noResponse // MARk: - TODO, change error
+    }
   }
 
   func createSession(requestToken: String) -> AnyPublisher<NewSession, DataTransferError> {

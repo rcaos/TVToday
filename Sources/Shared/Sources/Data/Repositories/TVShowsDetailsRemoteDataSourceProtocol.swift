730e2a29d6d4d1b14be083f56a95c03291c18bbd
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-05-13 11:33:13 -0500

    Add Missed Interface

--- Sources/Shared/Sources/Data/Repositories/TVShowsDetailsRemoteDataSourceProtocol.swift
+++ Sources/Shared/Sources/Data/Repositories/TVShowsDetailsRemoteDataSourceProtocol.swift
@@ -5,7 +5,8 @@
 //  Created by Jeans Ruiz on 13/05/22.
 //
 
-import Foundation
+import Combine
+import NetworkingInterface
 
 public protocol TVShowsDetailsRemoteDataSourceProtocol {
   func fetchTVShowDetails(with showId: Int) -> AnyPublisher<TVShowDetailDTO, DataTransferError>

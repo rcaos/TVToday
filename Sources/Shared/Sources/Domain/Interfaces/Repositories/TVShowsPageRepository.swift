83f4325680746eb9469724a042d2e931bbd53294
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-05-13 12:18:42 -0500

    Remove mappers to another folder

--- Sources/Shared/Sources/Domain/Interfaces/Repositories/TVShowsPageRepository.swift
+++ Sources/Shared/Sources/Domain/Interfaces/Repositories/TVShowsPageRepository.swift
@@ -42,21 +42,3 @@ public protocol TVShowsPageRepository {
   func fetchShowsByGenre(genreId: Int, page: Int) -> AnyPublisher<TVShowPage, DataTransferError>
   func searchShowsFor(query: String, page: Int) -> AnyPublisher<TVShowPage, DataTransferError>
 }
-
-public protocol TVShowPageMapperProtocol {
-  func mapTVShowPage(_ page: TVShowPageDTO, imageBasePath: String, imageSize: ImageSize) -> TVShowPage
-}
-
-public protocol TVShowDetailsMapperProtocol {
-  func mapTVShow(_ show: TVShowDetailDTO, imageBasePath: String, imageSize: ImageSize) -> TVShowDetail
-}
-
-public protocol AccountTVShowsDetailsMapperProtocol {
-  func mapActionResult(result: TVShowActionStatusDTO) -> TVShowActionStatus
-  func mapTVShowStatusResult(result: TVShowAccountStatusDTO) -> TVShowAccountStatus
-}
-
-public enum ImageSize: String {
-  case small = "w342"
-  case medium = "w780"
-}

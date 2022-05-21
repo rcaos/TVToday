3b1965fbf7db3aba98f8b7e3c24cea2ec0e4be4e
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-05-21 18:02:20 -0500

    Keep coordinator and state on Shared module

---
+++ Sources/Shared/Sources/Coordinator.swift
@@ -0,0 +1,30 @@
+//
+//  Coordinator.swift
+//  Shared
+//
+//  Created by Jeans Ruiz on 7/17/20.
+//
+
+import UIKit
+
+public protocol Coordinator: AnyObject {
+  func start(with step: Step)
+  func start()
+}
+
+public extension Coordinator {
+  func start(with step: Step = DefaultStep() ) { }
+  func start() { }
+}
+
+public protocol NavigationCoordinator: Coordinator {
+  var navigationController: UINavigationController { get }
+}
+
+// MARK: - Step Protocol
+/// Describe un posible estado de navegación dentro de un Coordinator
+public protocol Step { }
+
+public struct DefaultStep: Step {
+  public init() { }
+}

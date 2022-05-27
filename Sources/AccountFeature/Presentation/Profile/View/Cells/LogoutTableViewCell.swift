9dc1716b7d8c6ed9f540e6dc729302aeba0a3e53
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-05-27 17:45:07 -0500

    Conform with helper parent class

--- Sources/AccountFeature/Presentation/Profile/View/Cells/LogoutTableViewCell.swift
+++ Sources/AccountFeature/Presentation/Profile/View/Cells/LogoutTableViewCell.swift
@@ -1,26 +1,19 @@
 //
 //  LogoutTableViewCell.swift
-//  TVToday
+//  AccountFeature
 //
 //  Created by Jeans Ruiz on 6/22/20.
 //  Copyright © 2020 Jeans. All rights reserved.
 //
 
 import UIKit
+import UI
 
-class LogoutTableViewCell: UITableViewCell {
+class LogoutTableViewCell: NiblessTableViewCell {
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     setupUI()
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  override func awakeFromNib() {
-    super.awakeFromNib()
   }
 
   private func setupUI() {

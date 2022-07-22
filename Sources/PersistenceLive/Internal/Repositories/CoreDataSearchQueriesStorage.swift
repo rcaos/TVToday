dbce50535dd85e23933ac10949b4c850d98564ca
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-07-22 17:14:31 -0500

    Change error interface on live persistence module

--- Sources/PersistenceLive/Internal/Repositories/CoreDataSearchQueriesStorage.swift
+++ Sources/PersistenceLive/Internal/Repositories/CoreDataSearchQueriesStorage.swift
@@ -21,9 +21,9 @@ final class CoreDataSearchQueriesStorage {
 
 extension CoreDataSearchQueriesStorage: SearchLocalDataSource {
 
-  public func saveSearch(query: String, userId: Int) -> AnyPublisher<Void, CustomError> {
+  public func saveSearch(query: String, userId: Int) -> AnyPublisher<Void, ErrorEnvelope> {
     return Deferred { [store] in
-      return Future<Void, CustomError> { promise in
+      return Future<Void, ErrorEnvelope> { promise in
         store.delete(query: query)
         store.insert(query: query, userId: userId)
         promise(.success(()))
@@ -32,9 +32,9 @@ extension CoreDataSearchQueriesStorage: SearchLocalDataSource {
     .eraseToAnyPublisher()
   }
 
-  public func fetchRecentSearches(userId: Int) -> AnyPublisher<[SearchDLO], CustomError> {
+  public func fetchRecentSearches(userId: Int) -> AnyPublisher<[SearchDLO], ErrorEnvelope> {
     return Deferred { [store] in
-      return Future<[SearchDLO], CustomError> { promise in
+      return Future<[SearchDLO], ErrorEnvelope> { promise in
         let results = store.findAll(userId: userId).map { $0.toDomain() }
         promise(.success(results))
       }
